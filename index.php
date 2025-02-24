001310719bc212f6d3bd7418428141d5ef0c0eff
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-02-24 15:25:53 +0100

    m

--- index.php
+++ index.php
@@ -27,6 +27,8 @@
         <ul>
             <li>22 maart: Symfonie nr. 29 in A-groot KV 201 (30') voor 2 hobo's, 1 fagot, 2 hoorns en strijkers</li>
             <li>26 april: Symfonie nr. 4 in D KV 19 (10') en het klarinetconcert in A-groot KV 622 (29') voor 2 fluiten, 2 fagotten, 2 hoorns en strijkers. Solist: Marta Xavier (Portugal)</li>
+            <li>24 mei: Symfonie nr. 15 in G-groot, KV 124 (onder voorbehoud) en een nog te bepalen vioolconcert</li>
+            <li>27 september: Sinfonia Concertante in Eb KV 364 voor 2 hobo's, 1 fagot, 2 hoorns en strijkers. Solisten: Valentina Bernardone (viool, onder voorbehoud) en Anna Smith (altviool) </li>
         </ul>
         <p>Aan de programma's voor de volgende afleveringen wordt nog gewerkt. Waarschijnlijk gaan er dan naast symfonieën ook weer (solo)concerten van Mozart.</p>
         <p>Heb je belangstelling om eens mee te doen met Mozart op Zaterdag? We formeren een bezetting van serieuze en ervaren spelers die zich natuurlijk goed voorbereiden. We leveren op tijd (betekende) partijen als PDF's aan. Deelname is gratis; wel worden de deelnemers aan het eind om een vrijwillige bijdrage in de kosten van de zaal e.d. gevraagd. T.z.t. hopen we de professionele deelnemers ook wat te kunnen betalen.</p>
