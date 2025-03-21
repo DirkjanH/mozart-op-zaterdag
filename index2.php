ad7840a91843460451e7cca97815730d33703c10
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-03-21 14:58:49 +0100

    s

--- index2.php
+++ index2.php
@@ -10,9 +10,9 @@
 </head>
 
 <body>
-    <div class="w3-container w3-panel w3-mobile w3-row-padding" style="width: 100%; max-width: 1000px;">
+    <div class="w3-container w3-mobile" style="width: 100%; max-width: 1000px;">
         <?php include_once 'navigatie.htm'; ?>
-        <div class="w3-content w3-white w3-panel w3-rest">
+        <div class="w3-content w3-white w3-panel">
             <h2>Mozart op Zaterdag</h2>
                 <img
                     src="images/Mozart.jpg"
