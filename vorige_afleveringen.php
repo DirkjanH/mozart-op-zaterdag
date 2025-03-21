59ddf5d51869fa39ca74270070da16b924e1ce76
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-03-21 20:39:04 +0100

    m

--- vorige_afleveringen.php
+++ vorige_afleveringen.php
@@ -10,6 +10,7 @@
 
 <body>
     <div class="w3-content w3-white w3-panel">
+        <?php include_once 'navigatie.htm'; ?>
         <h2>Mozart op Zaterdag</h2>
         <h4>22 februari 2025 - symfonie nr. 25 KV 183</h4>
         <p><img src="images/MoZ_foto_buigen_Yo.jpg" alt="Mozart" class="w3-image w3-right w3-margin-left w3-margin-bottom" style="max-width: 500px;">De eerste keer vond plaats op zaterdag 22 februari in het <a href="/stadsklooster.htm">Stadsklooster</a>, met Mozarts 'kleine g-moll' symfonie nr. 25 KV 183 als onderwerp en een orkest van zo'n 30 strijkers en blazers als deelnemers, onder leiding van dirigent en initiatiefnemer <a href="https://www.horringa.net/index.php" target="_blank">Dirkjan Horringa</a>. Een 50-tal mensen kwam luisteren en was enthousiast.</p>
