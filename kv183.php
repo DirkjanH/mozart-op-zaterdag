88fa3d15b4857bce3d92314b88dccd69a3fa7c92
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-03-21 16:14:28 +0100

    m

--- kv183.php
+++ kv183.php
@@ -1,56 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Symfonie nr. 25 in g klein KV 183</title>
-        <link href="css/moz.css" rel="stylesheet" type="text/css">
-    </head>
-    <body>
-        <div class="w3-content w3-white w3-panel">
-            <?php include_once 'navigatie.htm'; ?>
-            <h3>Mozart op Zaterdag op 22 februari 2025:</h3>
-            <h1>Symfonie nr. 25 in g klein KV 183</h1>
-            <h4>Partijen</h4>
-            <ul style="column-count: 3;">
-                <li>
-                    <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237913-PMLP01544-01_Oboe_I.pdf" target="_blank">Hobo 1</a>
-                </li>
-                <li>
-                    <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237914-PMLP01544-02_Oboe_II.pdf" target="_blank">Hobo 2</a>
-                </li>
-                <li>
-                    <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237915-PMLP01544-03_Fagotto_I_II.pdf" target="_blank">Fagot 1/2</a>
-                </li>
-                <li>
-                    <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237916-PMLP01544-04_Corno_I.pdf" target="_blank">Hoorn 1</a>
-                </li>
-                <li>
-                    <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237917-PMLP01544-05_Corno_II.pdf" target="_blank">Hoorn 2</a>
-                </li>
-                <li>
-                    <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237918-PMLP01544-06_Corno_III.pdf" target="_blank">Hoorn 3</a>
-                </li>
-                <li>
-                    <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237919-PMLP01544-07_Corno_IV.pdf" target="_blank">Hoorn 4</a>
-                </li>
-                <li>
-                    <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237920-PMLP01544-08_Violino_I.pdf" target="_blank">Viool 1</a>
-                </li>
-                <li>
-                    <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237921-PMLP01544-09_Violino_II.pdf" target="_blank">Viool 2</a>
-                </li>
-                <li>
-                    <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237922-PMLP01544-10_Viola_I_II.pdf" target="_blank">Viola 1/2</a>
-                </li>
-                <li>
-                    <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237923-PMLP01544-11_Violoncelle_e_Basso.pdf" target="_blank">Cello / contrabas</a>
-                </li>
-                <li>
-                    <a href="\2025-02-22\KV183_Symfonie_25\Mozart 25 part + bet..pdf" target="_blank"><b>Betekende partituur</b></a></b>
-                </li>
-            </ul>
-            <p>NB. <b>Strijkers, neem SVP de streken uit de betekende partituur over in je eigen partij.</p>
-        </div>
-    </body>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Symfonie nr. 25 in g klein KV 183</title>
+    <link href="css/moz.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div class="w3-content w3-white w3-panel">
+        <?php include_once 'navigatie.htm'; ?>
+        <h3>Mozart op Zaterdag op 22 februari 2025:</h3>
+        <h1>Symfonie nr. 25 in g klein KV 183</h1>
+        <p>Op 22 februari stond Mozarts 25e symfonie KV 183 op het programma. Het is een van de maar twee symfonieën in mineur-toonsoorten die hij schreef. De andere is de beroemde 40e symfonie KV 550; beide staan in de dramatische toonsoort g klein. Nr. 25 wordt daarom ook wel de ‘kleine g mineur symfonie’ genoemd. Sinds de film Amadeus uitkwam, waarin in de opening de gejaagde muziek van het eerste deel klinkt, zou deze symfonie waarschijnlijk beter de 'Amadeus-symfonie' kunnen worden genoemd. Deze symfonie staat al meer dan halverwege Mozarts genummerde symfonieën. Het is de eerste, denk ik, die veel mensen rekenen tot een van zijn grote werken.</p>
+        <p>
+            Het werk dateert van oktober 1773, volgens sommige bronnen slechts enkele dagen nadat de 24e symfonie was voltooid. Het werk heeft paren van hobo's en fagotten, maar, ongebruikelijk voor die tijd, maar liefst vier hoorns en strijkers. Het werk heeft een totale duur van ongeveer 27 minuten en bestaat uit de volgende vier delen:
+            Allegro con brio, Andante, Menuetto en trio en Allegro</p>
+        <p>
+            Opvallend zijn de vier hoornpartijen, voor twee hoorns in hoog Bes en twee in G. Dat waren in Mozarts tijd natuurlijk natuurhoorns, die alleen de tonen uit de boventoonreeks konden spelen. Door deze stemmingen te kiezen kunnen de hoorns ook in een mineurtoonsoort worden ingezet.
+        </p>
+        <h4>Partijen</h4>
+        <ul style="column-count: 3;">
+            <li>
+                <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237913-PMLP01544-01_Oboe_I.pdf" target="_blank">Hobo 1</a>
+            </li>
+            <li>
+                <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237914-PMLP01544-02_Oboe_II.pdf" target="_blank">Hobo 2</a>
+            </li>
+            <li>
+                <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237915-PMLP01544-03_Fagotto_I_II.pdf" target="_blank">Fagot 1/2</a>
+            </li>
+            <li>
+                <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237916-PMLP01544-04_Corno_I.pdf" target="_blank">Hoorn 1</a>
+            </li>
+            <li>
+                <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237917-PMLP01544-05_Corno_II.pdf" target="_blank">Hoorn 2</a>
+            </li>
+            <li>
+                <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237918-PMLP01544-06_Corno_III.pdf" target="_blank">Hoorn 3</a>
+            </li>
+            <li>
+                <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237919-PMLP01544-07_Corno_IV.pdf" target="_blank">Hoorn 4</a>
+            </li>
+            <li>
+                <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237920-PMLP01544-08_Violino_I.pdf" target="_blank">Viool 1</a>
+            </li>
+            <li>
+                <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237921-PMLP01544-09_Violino_II.pdf" target="_blank">Viool 2</a>
+            </li>
+            <li>
+                <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237922-PMLP01544-10_Viola_I_II.pdf" target="_blank">Viola 1/2</a>
+            </li>
+            <li>
+                <a href="\2025-02-22\KV183_Symfonie_25\IMSLP237923-PMLP01544-11_Violoncelle_e_Basso.pdf" target="_blank">Cello / contrabas</a>
+            </li>
+            <li>
+                <a href="\2025-02-22\KV183_Symfonie_25\Mozart 25 part + bet..pdf" target="_blank"><b>Betekende partituur</b></a></b>
+            </li>
+        </ul>
+        <p>NB. <b>Strijkers, neem SVP de streken uit de betekende partituur over in je eigen partij.</p>
+    </div>
+</body>
+
 </html>
