4a2b21352767a4902b8b3e3aea32f9414bcb71cf
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-03-21 14:23:25 +0100

    m

--- Moz_2025-04-26.php
+++ Moz_2025-04-26.php
@@ -8,6 +8,7 @@
     </head>
     <body>
         <div class="w3-content w3-white w3-panel">
+            <?php include_once 'navigatie.htm'; ?>
             <h3>Mozart op Zaterdag op 26 april 2025:</h3>
             <h1>Symfonie nr. 4 in D KV 19 en het klarinetconcert in A-groot KV 622</h1>
             <p>
