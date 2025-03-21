fb93519d4c750a5d5f3087aff361a25297876f24
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-03-21 15:42:19 +0100

    p

---
+++ Moz_2025-05-24.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mozart op Zaterdag op 24 mei 2025</title>
+    <link href="css/moz.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div class="w3-content w3-white w3-panel">
+        <?php include_once 'navigatie.htm'; ?>
+        <h3>Mozart op Zaterdag op 24 mei 2025:</h3>
+        <h1>Symfonie nr. 15 in G-groot KV 124 en het vioolconcert nr. 4 in D KV 218</h1>
+        <p>
+            De derde aflevering van
+            <i>Mozart op Zaterdag</i>
+            vindt plaats op zaterdagochtend 24 mei. We werken dan aan Mozarts Symfonie nr. 10 in G KV 124 en het vierde vioolconcert in D KV 218. Mozart schreef zijn 10e symfonie in de eerste weken van 1772 in Salzburg. Hij was toen 16 jaar jong. De symfonie bestaat uit vier korte delen, een Allegro, een Andante, een Menuet en Trio, en een Presto, en duurt ongeveer 10 minuten. We spelen het zonder herhalingen.
+        </p>
+        <p>
+            Mozart schreef zijn vierde vioolconcert in D groot KV 218 in 1775 in Salzburg. Hij schijnt het oorspronkelijk gecomponeerd te hebben om zelf te spelen, maar na zijn vertrek uit het Salzburgse Hoforkest veranderde hij het concert zodat zijn opvolger in het hoforkest, Antonio Brunetti, het kon spelen. Het is discutabel of het concert boven Mozarts niveau lag of dat hij het met opzet moeilijk maakte voor Brunetti vanwege diens grotere vaardigheid. Het eerste deel is een virtuoos Allegro, terwijl het tweede deel, een Andante cantabile uit zangerige melodische lijnen bestaat. Het derde deel, een Rondeau, is vrolijk en vol plezier. Het concert duurt ongeveer 23 minuten.
+        </p>
+        <div class="onzichtbaar">
+            <h4>Partijen</h4>
+            <ul style="column-count: 3;">
+                <li>
+                    <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - hobo 1.pdf" target="_blank">Hobo 1</a>
+                </li>
+                <li>
+                    <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - hobo 2.pdf" target="_blank">Hobo 2</a>
+                </li>
+                <li>
+                    <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - hoorn 1.pdf" target="_blank">Hoorn 1</a>
+                </li>
+                <li>
+                    <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - hoorn 2.pdf" target="_blank">Hoorn 2</a>
+                </li>
+                <li>
+                    <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - viool 1.pdf" target="_blank">Viool 1</a>
+                </li>
+                <li>
+                    <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - viool 2.pdf" target="_blank">Viool 2</a>
+                </li>
+                <li>
+                    <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - viola.pdf" target="_blank">Altviool</a>
+                </li>
+                <li>
+                    <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - bas.pdf" target="_blank">Cello, fagot, contrabas</a>
+                </li>
+                <li>
+                    <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - partituur NMA.pdf" target="_blank">
+                        <b>nog niet betekende partituur</b>
+                    </a>
+                    </b>
+                </li>
+            </ul>
+            <p>
+                NB.
+                <b>
+                    Strijkers, neem SVP de streken uit de betekende partituur over in je eigen partij.
+            </p>
+        </div>
+    </div>
+</body>
+
+</html>
