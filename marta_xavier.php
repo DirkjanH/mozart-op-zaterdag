ec59ab8cfc29f4d3d93248a42901336f0c587b0d
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-04-05 15:02:24 +0200

    m

--- marta_xavier.php
+++ marta_xavier.php
@@ -12,7 +12,7 @@
     <div class="w3-content w3-white w3-panel">
         <?php include_once 'navigatie.htm'; ?>
         <h3>Marta Xavier</h3>
-        <p><img src="/images/Marta Xavier.jpg" alt="Marta Xavier, klarinet" width=300 style="float: left; margin-right: 20px;" class="w3-image">De Portugese klarinettiste Marta Xavier heeft het voorrecht gehad om op te treden op het Iberisch schiereiland, in Nederland, Frankrijk, Duitsland, de VS en Japan. Als solist heeft Xavier samengewerkt met het Cascais en Oeiras Kamerorkest, het Lisbon Youth Metropolitan Orchestra, Montalvo Wind Symphony en het Fullerton Symphony Orchestra.
+        <p><img src="/images/Marta Xavier.jpg" alt="Marta Xavier, klarinet" width=300 style="float: left; margin: 0 20px 20px 0;" class="w3-image">De Portugese klarinettiste Marta Xavier heeft het voorrecht gehad om op te treden op het Iberisch schiereiland, in Nederland, Frankrijk, Duitsland, de VS en Japan. Als solist heeft Xavier samengewerkt met het Cascais en Oeiras Kamerorkest, het Lisbon Youth Metropolitan Orchestra, Montalvo Wind Symphony en het Fullerton Symphony Orchestra.
             Als toegewijd kamermusicus is Marta lid van het Ébano Ensemble (Portugal), Quatuor Aeternum en Trio Claro (Nederland). Daarnaast heeft ze gewerkt met het Lisbon Metropolitan Orchestra, Cascais Symphony Orchestra, Sintra Municipal Orchestra en Trevo Ensemble. Daarnaast werkt ze meer gericht op de gemeenschap. Ze geeft lokale concerten op het Portugese platteland en bezoekt ze afgelegen basisscholen en verzorgingstehuizen. Naast de muziek is ze dichteres en heeft ze onlangs haar eerste dichtbundel gepubliceerd.</p>
     </div>
 </body>
