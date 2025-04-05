a83886ca72c2fa6dbe5f26c8eb43f0c2632da123
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-04-05 11:39:04 +0200

    m

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
 
 <body>
     <div class="w3-content w3-mobile w3-white w3-panel">
-         <?php include_once 'navigatie.htm'; ?>
-            <p><img
+        <?php include_once 'navigatie.htm'; ?>
+        <p><img
                 src="images/Mozart.jpg"
                 alt="Mozart"
                 class="w3-display-container w3-left w3-margin-right"
@@ -20,38 +20,37 @@
             <i>Mozart op Zaterdag</i>! Iedere maand komt een orkest van professionals en goede amateurs bijeen op een zaterdagochtend om een symfonie en/of een (solo)concert van Mozart te repeteren en meteen uit te voeren. Dirigent en initiatiefnemer is
             <a href="https://www.horringa.net/index.php" target="_blank">Dirkjan Horringa</a>. De eerste aflevering vond plaats op
             <a href="/vorige_afleveringen.php" target="_blank">zaterdag 22 februari</a>, met de 'kleine g-moll' symfonie nr. 25 KV 183 als onderwerp.
-            </p>
-            <h4>Meespelen?</h4>
-            <p>Heb je belangstelling om eens mee te doen met Mozart op Zaterdag? Het is mogelijk je op te geven voor de volgende afleveringen. Het is de bedoeling hier een maandelijkse traditie van te maken op de vierde zaterdag van de maand. Dus de volgende keren zijn op 26 april en 24 mei, en daarna weer op 27 september, 25 oktober en 22 november. We werken in de
-                <a href="/marnixzaal.php" target="_blank">Marnixzaal</a> van de vroegere Utrechtse Muziekschool aan het Domplein of in het
-                <a href="/stadsklooster.php" target="_blank">Stadsklooster</a> in de wijk Lombok. Er wordt koffie en thee geschonken in de zaal.
-            </p>
-            <h4>Tijden</h4>
-            <p>We werken als volgt: binnenkomen om 9:45, repeteren van 10:00 tot 12:30 met een korte pauze. Om 13:00 sluiten we af met een concertje van een half uur.
-            </p>
-            <h4>Meld je aan met het formulier</h4>
-            <p>We hopen dat je deze geweldige muziek met ons wilt spelen! We formeren een bezetting van serieuze en ervaren spelers die zich natuurlijk goed voorbereiden. We leveren op tijd (betekende) partijen als PDF's aan.
-                <a href="https://forms.gle/49YfM2dSn8AYpzfE8" target="_blank">In dit formulier kun je aangeven of en wanneer je wilt meedoen</a>. Mogelijk zijn er meer inschrijvingen dan plaatsen.
-                <b>Reageer daarom snel!</b> Deelname is gratis; wel worden de deelnemers aan het eind om een vrijwillige bijdrage in de kosten van de zaal e.d. gevraagd. T.z.t. hopen we de professionele deelnemers ook wat te kunnen betalen.
-            </p>
-            <h4>Voor de komende keren staan deze stukken op het programma:</h4>
-            <ul>
-                <li>
-                    <b>26 april (koningsdag)</b> in het
-                    <a href="/stadsklooster.php" target="_blank">Stadsklooster</a>: Symfonie nr. 4 in D KV 19 (10') en het klarinetconcert in A-groot KV 622 (29') voor 2 fluiten, 2 fagotten, 2 hoorns en strijkers. Solist: Marta Xavier (Portugal)
-                </li>.
-                <a href="Moz_2025-04-26.php">Meer info & partijen vind je hier</a>
-                <li>
-                    <b>24 mei</b> in de
-                    <a href="/marnixzaal.php" target="_blank">Marnixzaal</a>: Symfonie nr. 15 in G-groot KV 124 (14') voor 2 hobo's, 1 fagot, 2 hoorns en strijkers en het vioolconcert nr. 4 in D KV 218 (solist: Evelyn Tjon-En-Fa). <a href="Moz_2025-05-24.php">Meer info & partijen vind je hier</a>
-                </li>
-                <li>
-                    <b>27 september</b> in de
-                    <a href="/marnixzaal.php" target="_blank">Marnixzaal</a>: Sinfonia Concertante in Eb KV 364 voor 2 hobo's, 1 fagot, 2 hoorns en strijkers. Solisten: Valentina Bernardone (viool, onder voorbehoud) en Anna Smith (altviool)
-                </li>
-            </ul>
-            <p>Aan de programma's voor de volgende afleveringen wordt nog gewerkt. Waarschijnlijk gaan er dan naast symfonieën ook weer (solo)concerten van Mozart. Er is al contact met professionele pianisten die graag een pianoconcert van Mozart voor hun rekening willen nemen</p>
-        </div>
+        </p>
+        <h4>Meespelen?</h4>
+        <p>Heb je belangstelling om eens mee te doen met Mozart op Zaterdag? Het is mogelijk je op te geven voor de volgende afleveringen. Het is de bedoeling hier een maandelijkse traditie van te maken op de vierde zaterdag van de maand. Dus de volgende keren zijn op 26 april en 24 mei, en daarna weer op 27 september, 25 oktober en 22 november. We werken in de
+            <a href="/marnixzaal.php" target="_blank">Marnixzaal</a> van de vroegere Utrechtse Muziekschool aan het Domplein of in het
+            <a href="/stadsklooster.php" target="_blank">Stadsklooster</a> in de wijk Lombok. Er wordt koffie en thee geschonken in de zaal.
+        </p>
+        <h4>Tijden</h4>
+        <p>We werken als volgt: binnenkomen om 9:45, repeteren van 10:00 tot 12:30 met een korte pauze. Om 13:00 sluiten we af met een concertje van een half uur.
+        </p>
+        <h4>Meld je aan met het formulier</h4>
+        <p>We hopen dat je deze geweldige muziek met ons wilt spelen! We formeren een bezetting van serieuze en ervaren spelers die zich natuurlijk goed voorbereiden. We leveren op tijd (betekende) partijen als PDF's aan.
+            <a href="https://forms.gle/49YfM2dSn8AYpzfE8" target="_blank">In dit formulier kun je aangeven of en wanneer je wilt meedoen</a>. Mogelijk zijn er meer inschrijvingen dan plaatsen.
+            <b>Reageer daarom snel!</b> Deelname is gratis; wel worden de deelnemers aan het eind om een vrijwillige bijdrage in de kosten van de zaal e.d. gevraagd. T.z.t. hopen we de professionele deelnemers ook wat te kunnen betalen.
+        </p>
+        <h4>Voor de komende keren staan deze stukken op het programma:</h4>
+        <ul>
+            <li>
+                <b>26 april (koningsdag)</b> in het
+                <a href="/stadsklooster.php" target="_blank">Stadsklooster</a>: Symfonie nr. 4 in D KV 19 (10') en het klarinetconcert in A-groot KV 622 (29') voor 2 fluiten, 2 fagotten, 2 hoorns en strijkers. Solist: Marta Xavier (Portugal). <a href="Moz_2025-04-26.php">Meer info & partijen vind je hier</a>
+            </li>
+            <li>
+                <b>24 mei</b> in de
+                <a href="/marnixzaal.php" target="_blank">Marnixzaal</a>: Symfonie nr. 15 in G-groot KV 124 (14') voor 2 hobo's, 1 fagot, 2 hoorns en strijkers en het vioolconcert nr. 4 in D KV 218 (solist: Evelyn Tjon-En-Fa). <a href="Moz_2025-05-24.php">Meer info & partijen vind je hier</a>
+            </li>
+            <li>
+                <b>27 september</b> in de
+                <a href="/marnixzaal.php" target="_blank">Marnixzaal</a>: Sinfonia Concertante in Eb KV 364 voor 2 hobo's, 1 fagot, 2 hoorns en strijkers. Solisten: Valentina Bernardone (viool, onder voorbehoud) en Anna Smith (altviool)
+            </li>
+        </ul>
+        <p>Aan de programma's voor de volgende afleveringen wordt nog gewerkt. Waarschijnlijk gaan er dan naast symfonieën ook weer (solo)concerten van Mozart. Er is al contact met professionele pianisten die graag een pianoconcert van Mozart voor hun rekening willen nemen</p>
+    </div>
 </body>
 
 </html>
