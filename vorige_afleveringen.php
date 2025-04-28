2873c5317b11b63cdcbd843063909b37f4419729
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-04-28 17:49:55 +0200

    Refactor code structure for improved readability and maintainability

--- vorige_afleveringen.php
+++ vorige_afleveringen.php
@@ -22,15 +22,18 @@
             onder leiding van dirigent en initiatiefnemer <a href="https://www.horringa.net/index.php"
                 target="_blank">Dirkjan Horringa</a>. Een 50-tal mensen kwam luisteren en was enthousiast.</p>
         <h4>26 april 2025 - Symfonie nr. 4 in D KV 19 en het klarinetconcert in A-groot KV 622</h4>
-        <p>Op 26 april (koningsdag) vond de <a href="2025-04-26\Moz_2025-04-26.php" target="_blank"></a>tweede
-            aflevering plaats in het
-            <a href="/stadsklooster.php" target="_blank">Stadsklooster</a>. Gespeeld werd Symfonie nr. 4 in D KV 19
-            (10') en het
-            klarinetconcert in A-groot KV 622 (29') voor 2 fluiten, 2 fagotten, 2 hoorns en strijkers. Soliste was <a
-                href="\2025-04-26\marta_xavier.php" target="_blank">Marta
-                Xavier (Portugal)</a>. Er speelden 26 mensen in het orkest. Er waren een kleine 40 bezoekers.
-        </p>
-
+        <div style="clear: both;" class="w3-panel">
+            <p><img src="/images/2025-04-26 Mozart op Zaterdag klarinetconcert.jpg" alt="Mozart klarinetconcert"
+                    class="w3-image w3-right w3-margin-left w3-margin-bottom" style (koningsdag) vond de <a
+                    href="2025-04-26\Moz_2025-04-26.php" target="_blank"></a>tweede
+                aflevering plaats in het
+                <a href="/stadsklooster.php" target="_blank">Stadsklooster</a>. Gespeeld werd Symfonie nr. 4 in D KV 19
+                (10') en het
+                klarinetconcert in A-groot KV 622 (29') voor 2 fluiten, 2 fagotten, 2 hoorns en strijkers. Soliste was
+                <a href="\2025-04-26\marta_xavier.php" target="_blank">Marta
+                    Xavier (Portugal)</a>. Er speelden 26 mensen in het orkest. Er waren een kleine 40 bezoekers.
+            </p>
+        </div>
     </div>
 </body>
 
