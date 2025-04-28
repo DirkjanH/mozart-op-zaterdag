32d4072d042a8fdbf5b28e9199611a83b20334e5
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-04-28 15:40:39 +0200

    Fix CSS link path and update concert title in Moz_2025-05-24.php

--- 2025-05-24/Moz_2025-05-24.php
+++ 2025-05-24/Moz_2025-05-24.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mozart op Zaterdag op 24 mei 2025</title>
-    <link href="/css/moz.css" rel="stylesheet" type="text/css">
+    <link href="css/moz.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
     <p class="w3-content w3-white w3-panel">
         <?php include_once 'navigatie.htm'; ?>
     <h3>24 mei 2025:</h3>
-    <h2>Symfonie nr. 14 in G-groot KV 124 en het vioolconcert nr. 4 in D KV 218</h2>
+    <h2>Vioolconcert nr. 4 in D KV 218</h2>
     <p>
         De derde aflevering van
         <i>Mozart op Zaterdag</i>
@@ -26,7 +26,7 @@
         Andante cantabile uit zangerige melodische lijnen bestaat. Het derde deel, een Rondeau, is vrolijk en vol
         plezier. Het concert duurt ongeveer 25 minuten.
     </p>
-    <p><img src="evelyn.jpg" alt="Evelyn Tjon-En-Fa" class="w3-image we-left w3-marginright">Soliste is Evelyn
+    <p><img src="evelyn.jpg" alt="Evelyn Tjon-En-Fa" class="w3-image we-left w3-margin-right">Soliste is Evelyn
         Tjon-En-Fa. Zij werkte
         eerder mee als concertmeester. In 2001 studeerde Evelyn cum laude af als uitvoerend musicus aan het
         conservatorium in Tilburg (Fontys Hogescholen). Evelyn heeft sinds 1997 regelmatig opgetreden als solist met
