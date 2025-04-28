7bcbf0a859074df4079b5f4a9ee3b686867e6e73
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-04-28 17:50:58 +0200

    Fix formatting and improve structure in concert details section

--- vorige_afleveringen.php
+++ vorige_afleveringen.php
@@ -21,10 +21,10 @@
             g-moll' symfonie nr. 25 KV 183 als onderwerp en een orkest van zo'n 30 strijkers en blazers als deelnemers,
             onder leiding van dirigent en initiatiefnemer <a href="https://www.horringa.net/index.php"
                 target="_blank">Dirkjan Horringa</a>. Een 50-tal mensen kwam luisteren en was enthousiast.</p>
-        <h4>26 april 2025 - Symfonie nr. 4 in D KV 19 en het klarinetconcert in A-groot KV 622</h4>
         <div style="clear: both;" class="w3-panel">
+            <h4>26 april 2025 - Symfonie nr. 4 in D KV 19 en het klarinetconcert in A-groot KV 622</h4>
             <p><img src="/images/2025-04-26 Mozart op Zaterdag klarinetconcert.jpg" alt="Mozart klarinetconcert"
-                    class="w3-image w3-right w3-margin-left w3-margin-bottom" style (koningsdag) vond de <a
+                    class="w3-image w3-right w3-margin-left w3-margin-bottom">Op 26 april (koningsdag) vond de <a
                     href="2025-04-26\Moz_2025-04-26.php" target="_blank"></a>tweede
                 aflevering plaats in het
                 <a href="/stadsklooster.php" target="_blank">Stadsklooster</a>. Gespeeld werd Symfonie nr. 4 in D KV 19
