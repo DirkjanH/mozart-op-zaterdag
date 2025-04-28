518f07b27c80dd45dac281f19840e3d10b35581a
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-04-28 14:54:37 +0200

    Refactor code structure for improved readability and maintainability

--- 2025-05-24/Moz_2025-05-24.php
+++ 2025-05-24/Moz_2025-05-24.php
@@ -9,68 +9,73 @@
 </head>
 
 <body>
-    <div class="w3-content w3-white w3-panel">
+    <p class="w3-content w3-white w3-panel">
         <?php include_once 'navigatie.htm'; ?>
-        <h3>24 mei 2025:</h3>
-        <h2>Symfonie nr. 15 in G-groot KV 124 en het vioolconcert nr. 4 in D KV 218</h2>
+    <h3>24 mei 2025:</h3>
+    <h2>Symfonie nr. 14 in G-groot KV 124 en het vioolconcert nr. 4 in D KV 218</h2>
+    <p>
+        De derde aflevering van
+        <i>Mozart op Zaterdag</i>
+        vindt plaats op zaterdagochtend 24 mei. We werken dan aan Mozarts Symfonie nr. 14 in G KV 124 en het vierde
+        vioolconcert in D KV 218. Mozart schreef zijn 10e symfonie in de eerste weken van 1772 in Salzburg. Hij was
+        toen 16 jaar jong. De symfonie bestaat uit vier korte delen, een Allegro, een Andante, een Menuet en Trio,
+        en een Presto, en duurt ongeveer 10 minuten. We spelen het zonder herhalingen.
+    </p>
+    <p>
+        Mozart schreef zijn vierde vioolconcert in D groot KV 218 in 1775 in Salzburg. Hij schijnt het
+        oorspronkelijk gecomponeerd te hebben om zelf te spelen, maar na zijn vertrek uit het Salzburgse Hoforkest
+        veranderde hij het concert zodat zijn opvolger in het hoforkest, Antonio Brunetti, het kon spelen. Het is
+        discutabel of het concert boven Mozarts niveau lag of dat hij het met opzet moeilijk maakte voor Brunetti
+        vanwege diens grotere vaardigheid. Het eerste deel is een virtuoos Allegro, terwijl het tweede deel, een
+        Andante cantabile uit zangerige melodische lijnen bestaat. Het derde deel, een Rondeau, is vrolijk en vol
+        plezier. Het concert duurt ongeveer 25 minuten. </p>
+    <p>Soliste in het vioolconcert is Evelyn Tjon-En-Fa. Zij werkte
+        eerder mee als concertmeester. In 2001 studeerde Evelyn cum laude af als uitvoerend musicus aan het
+        conservatorium in Tilburg (Fontys Hogescholen). Evelyn heeft sinds 1997 regelmatig opgetreden als solist met
+        orkesten waarvan zij deel uitmaakt en heeft een ruime ervaring in kamermuziek. Evelyn heeft parallel aan haar
+        conservatoriumstudie Nederlands recht gestudeerd. Zij werkt sinds 2001 als advocaat.
+    </p>
+    <div class="onzichtbaar">
+        <h4>Partijen</h4>
+        <ul style="column-count: 3;">
+            <li>
+                <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - hobo 1.pdf" target="_blank">Hobo 1</a>
+            </li>
+            <li>
+                <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - hobo 2.pdf" target="_blank">Hobo 2</a>
+            </li>
+            <li>
+                <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - hoorn 1.pdf" target="_blank">Hoorn 1</a>
+            </li>
+            <li>
+                <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - hoorn 2.pdf" target="_blank">Hoorn 2</a>
+            </li>
+            <li>
+                <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - viool 1.pdf" target="_blank">Viool 1</a>
+            </li>
+            <li>
+                <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - viool 2.pdf" target="_blank">Viool 2</a>
+            </li>
+            <li>
+                <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - viola.pdf" target="_blank">Altviool</a>
+            </li>
+            <li>
+                <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - bas.pdf" target="_blank">Cello, fagot,
+                    contrabas</a>
+            </li>
+            <li>
+                <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - partituur NMA.pdf" target="_blank">
+                    <b>nog niet betekende partituur</b>
+                </a>
+                </b>
+            </li>
+        </ul>
         <p>
-            De derde aflevering van
-            <i>Mozart op Zaterdag</i>
-            vindt plaats op zaterdagochtend 24 mei. We werken dan aan Mozarts Symfonie nr. 10 in G KV 124 en het vierde
-            vioolconcert in D KV 218. Mozart schreef zijn 10e symfonie in de eerste weken van 1772 in Salzburg. Hij was
-            toen 16 jaar jong. De symfonie bestaat uit vier korte delen, een Allegro, een Andante, een Menuet en Trio,
-            en een Presto, en duurt ongeveer 10 minuten. We spelen het zonder herhalingen.
+            NB.
+            <b>
+                Strijkers, neem SVP de streken uit de betekende partituur over in je eigen partij.
         </p>
-        <p>
-            Mozart schreef zijn vierde vioolconcert in D groot KV 218 in 1775 in Salzburg. Hij schijnt het
-            oorspronkelijk gecomponeerd te hebben om zelf te spelen, maar na zijn vertrek uit het Salzburgse Hoforkest
-            veranderde hij het concert zodat zijn opvolger in het hoforkest, Antonio Brunetti, het kon spelen. Het is
-            discutabel of het concert boven Mozarts niveau lag of dat hij het met opzet moeilijk maakte voor Brunetti
-            vanwege diens grotere vaardigheid. Het eerste deel is een virtuoos Allegro, terwijl het tweede deel, een
-            Andante cantabile uit zangerige melodische lijnen bestaat. Het derde deel, een Rondeau, is vrolijk en vol
-            plezier. Het concert duurt ongeveer 23 minuten. Soliste in het vioolconcert is Evelyn Tjon-En-Fa.
-        </p>
-        <div class="onzichtbaar">
-            <h4>Partijen</h4>
-            <ul style="column-count: 3;">
-                <li>
-                    <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - hobo 1.pdf" target="_blank">Hobo 1</a>
-                </li>
-                <li>
-                    <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - hobo 2.pdf" target="_blank">Hobo 2</a>
-                </li>
-                <li>
-                    <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - hoorn 1.pdf" target="_blank">Hoorn 1</a>
-                </li>
-                <li>
-                    <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - hoorn 2.pdf" target="_blank">Hoorn 2</a>
-                </li>
-                <li>
-                    <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - viool 1.pdf" target="_blank">Viool 1</a>
-                </li>
-                <li>
-                    <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - viool 2.pdf" target="_blank">Viool 2</a>
-                </li>
-                <li>
-                    <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - viola.pdf" target="_blank">Altviool</a>
-                </li>
-                <li>
-                    <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - bas.pdf" target="_blank">Cello, fagot,
-                        contrabas</a>
-                </li>
-                <li>
-                    <a href="\2025-03-22\Mozart - Symfonie nr. 29 in A KV 201 - partituur NMA.pdf" target="_blank">
-                        <b>nog niet betekende partituur</b>
-                    </a>
-                    </b>
-                </li>
-            </ul>
-            <p>
-                NB.
-                <b>
-                    Strijkers, neem SVP de streken uit de betekende partituur over in je eigen partij.
-            </p>
-        </div>
+    </div>
     </div>
 </body>
 
