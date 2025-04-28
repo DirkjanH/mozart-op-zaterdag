0fb8c9f9ef9244a2ad3ef158498e19661ec640cb
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-04-28 17:52:10 +0200

    Fix image alignment and improve formatting in concert details section

--- vorige_afleveringen.php
+++ vorige_afleveringen.php
@@ -24,8 +24,8 @@
         <div style="clear: both;" class="w3-panel">
             <h4>26 april 2025 - Symfonie nr. 4 in D KV 19 en het klarinetconcert in A-groot KV 622</h4>
             <p><img src="/images/2025-04-26 Mozart op Zaterdag klarinetconcert.jpg" alt="Mozart klarinetconcert"
-                    class="w3-image w3-right w3-margin-left w3-margin-bottom">Op 26 april (koningsdag) vond de <a
-                    href="2025-04-26\Moz_2025-04-26.php" target="_blank"></a>tweede
+                    class="w3-image w3-left w3-margin-right w3-margin-bottom" style="max-width: 500px;">Op 26 april
+                (koningsdag) vond de <a href="2025-04-26\Moz_2025-04-26.php" target="_blank"></a>tweede
                 aflevering plaats in het
                 <a href="/stadsklooster.php" target="_blank">Stadsklooster</a>. Gespeeld werd Symfonie nr. 4 in D KV 19
                 (10') en het
