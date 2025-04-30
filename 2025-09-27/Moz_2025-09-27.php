24ad172d049978125c58421b41f5af448f08eb68
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-04-30 15:46:26 +0200

    Fix layout by adding line breaks for soloists' names in concert details

--- 2025-09-27/Moz_2025-09-27.php
+++ 2025-09-27/Moz_2025-09-27.php
@@ -26,10 +26,12 @@
         <div style="clear: both; display: table;">
             <div style="display: table-cell;" class="w3-small w3-center"><img
                     src="/2025-09-27/valentina-bernardone-nederlands-.ed8ef09e.fill-1280x1280.jpg" width="350"
-                    alt="Valentina Bernardone" class="w3-image w3-panel w3-left geenlijn">Valentina Bernardone, viool
+                    alt="Valentina Bernardone" class="w3-image w3-panel w3-left geenlijn"><br>Valentina Bernardone,
+                viool
             </div>
             <div style="display: table-cell;" class="w3-small w3-center"><img src="/2025-09-27/Anna-Smith-vierkant-.jpg"
-                    width="350" alt="Anna Smith" class="w3-image w3-panel w3-left geenlijn">Anna Smith, altviool</div>
+                    width="350" alt="Anna Smith" class="w3-image w3-panel w3-left geenlijn"><br>Anna Smith, altviool
+            </div>
         </div>
         </p>
         <div class="" style="clear: both;">
