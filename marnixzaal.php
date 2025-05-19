edf078817d75605d392bfca8da52743ddb44af16
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-05-19 16:08:49 +0200

    m

--- marnixzaal.php
+++ marnixzaal.php
@@ -35,41 +35,23 @@
                 <h4 class="a11y-option__header"> Gehandicapenparkeerplaats</h4>
                 <p><a href="https://maps.app.goo.gl/HFHYEnwMA9yRWCiZA">Voetiusstraat</a>: 3<br><a href="https://maps.app.goo.gl/yTYvzER7qYij2KC2A">Domstraat</a>: 4<br><a href="https://maps.app.goo.gl/55Qfy7oxnui61cCy5">Hamburgerstraat</a>: 18</p>
             </div>
+            <h3 class="a11y-option__label">Fietsenstalling</h3>
+            <div id="paragraph-428403" class="a11y-option__content l-alternative-text">
+                <p>Aanwezig<br>Locatie: voor de entree</p>
+            </div>
+            <h2 class="label-above">Toegankelijkheid</h2>
+            <h3 class="a11y-option__label">Ingangen</h3>
+            <h4 class="a11y-option__header"> Hoofdingang</h4>
+            <p>Rolstoelvriendelijk.</p>
+            <p>Locatie: aan Domplein in pand Utrechts centrum voor de kunsten.</p>
+            <p>Type: schuifdeuren die automatisch openen</p>
+            <h3 class="a11y-option__label">Liften</h3>
+            <p>Aanwezig<br><strong>Locatie: </strong>Bij binnenkomst rechtdoor, na de toiletten de gang door naar links.</p>
+            <p>De Marnixzaal bevindt zich op de eerste etage.</p>
+            <h3 class="a11y-option__label">Rolstoelvriendelijkheid</h3>
+            <h4 class="a11y-option__header"> Integraal toegankelijke toiletruimte</h4>
+            <p>Aanwezig<br>Locatie: Op de begane grond, vanaf de ingang rechtdoor aan de linkerkant.</p>
         </div>
-        <h3 class="a11y-option__label">Fietsenstalling</h3>
-        <div id="paragraph-428403" class="a11y-option__content l-alternative-text">
-            <p>Aanwezig<br>Locatie: voor de entree</p>
-        </div>
-    </div>
-    <h2 class="label-above">Toegankelijkheid</h2>
-    <h3 class="a11y-option__label">Ingangen</h3>
-    <h4 class="a11y-option__header"> Hoofdingang</h4>
-    <p>Rolstoelvriendelijk.</p>
-    <p>Locatie: aan Domplein in pand Utrechts centrum voor de kunsten.</p>
-    <p>Type: schuifdeuren die automatisch openen</p>
-    <h3 class="a11y-option__label">Liften</h3>
-    <p>Aanwezig<br><strong>Locatie: </strong>Bij binnenkomst rechtdoor, na de toiletten de gang door naar links.</p>
-    <p>De Marnixzaal bevindt zich op de eerste etage.</p>
-    <h3 class="a11y-option__label">Rolstoelvriendelijkheid</h3>
-    <h4 class="a11y-option__header"> Integraal toegankelijke toiletruimte</h4>
-    <p>Aanwezig<br>Locatie: Op de begane grond, vanaf de ingang rechtdoor aan de linkerkant.</p>
-    </div>
-    </div>
-    <div id="paragraph-428419" class="content-block">
-        <div class="content-block__inner">
-            <ul class="grid grid--3 grid--with-gaps">
-                <li class="grid__item"> <a href="https://www.uu.nl/sites/default/files/20241204_Domplein_4-5%20%2884%20van%2085%29-min.jpg" class="l-full-width-link" data-caption="Vooraanzicht Domplein 4" data-fancybox="gallery-428419"><img loading="lazy" src="/sites/default/files/styles/image_270x180/public/20241204_Domplein_4-5%20%2884%20van%2085%29-min.jpg?mt=1737563024&amp;itok=grpbfR_N" width="270" height="180" alt="Vooraanzicht Domplein 4"></a></li>
-                <li class="grid__item"> <a href="https://www.uu.nl/sites/default/files/20241204_Domplein_4-5%20%281%20van%2085%29-min.jpg?mt=1737562758" class="l-full-width-link" data-caption="Ingang Domplein 4" data-fancybox="gallery-428419"><img loading="lazy" src="/sites/default/files/styles/image_270x180/public/20241204_Domplein_4-5%20%281%20van%2085%29-min.jpg?mt=1737563024&amp;itok=AgA-S4ru" width="270" height="180" alt="Ingang Domplein 4"></a></li>
-                <li class="grid__item"> <a href="https://www.uu.nl/sites/default/files/20241204_Domplein_4-5%20%2850%20van%2085%29-min.jpg?mt=1737562827" class="l-full-width-link" data-caption="Lift - gesloten" data-fancybox="gallery-428419"><img loading="lazy" src="/sites/default/files/styles/image_270x180/public/20241204_Domplein_4-5%20%2850%20van%2085%29-min.jpg?mt=1737563024&amp;itok=VY9scaqt" width="270" height="180" alt="Lift - gesloten"></a></li>
-                <li class="grid__item"> <a href="https://www.uu.nl/sites/default/files/20241204_Domplein_4-5%20%2852%20van%2085%29-min.jpg?mt=1737562863" class="l-full-width-link" data-caption="Lift - geopend" data-fancybox="gallery-428419"><img loading="lazy" src="/sites/default/files/styles/image_270x180/public/20241204_Domplein_4-5%20%2852%20van%2085%29-min.jpg?mt=1737563028&amp;itok=ghAy5DkB" width="270" height="180" alt="Lift - geopend"></a></li>
-                <li class="grid__item"> <a href="https://www.uu.nl/sites/default/files/20241204_Domplein_4-5%20%2822%20van%2085%29-min.jpg?mt=1737562907" class="l-full-width-link" data-caption="Marnixzaal" data-fancybox="gallery-428419"><img loading="lazy" src="/sites/default/files/styles/image_270x180/public/20241204_Domplein_4-5%20%2822%20van%2085%29-min.jpg?mt=1737563028&amp;itok=YVZTqS6x" width="270" height="180" alt="Marnixzaal"></a></li>
-                <li class="grid__item"> <a href="https://www.uu.nl/sites/default/files/20241204_Domplein_4-5%20%287%20van%2085%29-min.jpg?mt=1737562940" class="l-full-width-link" data-caption="Ingang met helling" data-fancybox="gallery-428419"><img loading="lazy" src="/sites/default/files/styles/image_270x180/public/20241204_Domplein_4-5%20%287%20van%2085%29-min.jpg?mt=1737563028&amp;itok=MNtNnu9z" width="270" height="180" alt="Ingang met helling"></a></li>
-                <li class="grid__item"> <a href="https://www.uu.nl/sites/default/files/20241204_Domplein_4-5%20%284%20van%2085%29-min.jpg?mt=1737562996" class="l-full-width-link" data-caption="Bewegwijzering" data-fancybox="gallery-428419"><img loading="lazy" src="/sites/default/files/styles/image_270x180/public/20241204_Domplein_4-5%20%284%20van%2085%29-min.jpg?mt=1737563028&amp;itok=jt4qDzmZ" width="270" height="180" alt="Bewegwijzering"></a></li>
-            </ul>
-        </div>
-    </div>
-    </div>
-    </div>
 </body>
 
 </html>
