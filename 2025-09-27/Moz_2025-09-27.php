3375176ac540c22f189613d4a1d665f572523c8d
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-06-17 17:32:35 +0200

    Add participant details for the Sinfonia concertante in Eb KV 364

--- 2025-09-27/Moz_2025-09-27.php
+++ 2025-09-27/Moz_2025-09-27.php
@@ -34,7 +34,246 @@
             </div>
         </div>
         </p>
+
         <div class="clear";">
+            <h4>Deelnemers</h4>
+    <table cellspacing="0" cellpadding="0">
+        <thead>
+                <th>voornaam</td>
+                <th>achternaam</td>
+                <th>instrument</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th id="1691098179R1" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">2</div>
+                </th>
+                <td>Liesbeth</td>
+                <td>Feikema</td>
+                <td>hobo 1</td>
+            </tr>
+            <tr>
+                <th id="1691098179R2" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">3</div>
+                </th>
+                <td>Rineke</td>
+                <td>Boerwinkel</td>
+                <td>hobo 2</td>
+            </tr>
+            <tr>
+                <th id="1691098179R3" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">4</div>
+                </th>
+                <td>Kyra</td>
+                <td>Brakkee</td>
+                <td>fagot</td>
+            </tr>
+            <tr>
+                <th id="1691098179R4" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">5</div>
+                </th>
+                <td>Ger</td>
+                <td>Otten</td>
+                <td>hoorn 1</td>
+            </tr>
+            <tr>
+                <th id="1691098179R5" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">6</div>
+                </th>
+                <td>Stefaan</td>
+                <td>Brakkee</td>
+                <td>hoorn 2</td>
+            </tr>
+            <tr>
+                <th id="1691098179R6" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">7</div>
+                </th>
+                <td>Saskia</td>
+                <td>Luijendijk</td>
+                <td>viool 1</td>
+            </tr>
+            <tr>
+                <th id="1691098179R7" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">8</div>
+                </th>
+                <td>Carla</td>
+                <td>Westhof</td>
+                <td class="s2 softmerge" dir="ltr">
+                    <div class="softmerge-inner" style="width:148px;left:-1px">viool 1 (concertmeester)</div>
+                </td>
+            </tr>
+            <tr>
+                <th id="1691098179R8" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">9</div>
+                </th>
+                <td class="s1">Corinne</td>
+                <td class="s1">Britzel</td>
+                <td>viool 1</td>
+            </tr>
+            <tr>
+                <th id="1691098179R9" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">10</div>
+                </th>
+                <td>Sophie</td>
+                <td>Vermond</td>
+                <td>viool 1</td>
+            </tr>
+            <tr>
+                <th id="1691098179R10" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">11</div>
+                </th>
+                <td>Ruben </td>
+                <td>Faas</td>
+                <td>viool 1</td>
+            </tr>
+            <tr>
+                <th id="1691098179R11" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">12</div>
+                </th>
+                <td class="s1">Jan</td>
+                <td class="s1">Prins</td>
+                <td class="s1">viool 2</td>
+            </tr>
+            <tr>
+                <th id="1691098179R12" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">13</div>
+                </th>
+                <td>Lidwien </td>
+                <td>Geertjens</td>
+                <td>viool 2</td>
+            </tr>
+            <tr>
+                <th id="1691098179R13" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">14</div>
+                </th>
+                <td>Maria</td>
+                <td>Voorhout</td>
+                <td>viool 2</td>
+            </tr>
+            <tr>
+                <th id="1691098179R14" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">15</div>
+                </th>
+                <td>Adriaan</td>
+                <td>Mol</td>
+                <td>viool 2</td>
+            </tr>
+            <tr>
+                <th id="1691098179R15" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">16</div>
+                </th>
+                <td>Sylvia</td>
+                <td>Keijser</td>
+                <td class="s2 softmerge" dir="ltr">
+                    <div class="softmerge-inner" style="width:119px;left:-1px">viool 2 (aanvoerder)</div>
+                </td>
+            </tr>
+            <tr>
+                <th id="1691098179R16" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">17</div>
+                </th>
+                <td>Saskia</td>
+                <td>Wieberdink</td>
+                <td>altviool 1</td>
+            </tr>
+            <tr>
+                <th id="1691098179R17" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">18</div>
+                </th>
+                <td>Sytse</td>
+                <td>Dotinga</td>
+                <td>altviool 1</td>
+            </tr>
+            <tr>
+                <th id="1691098179R18" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">19</div>
+                </th>
+                <td>Mirjam</td>
+                <td>van den Berg</td>
+                <td>altviool 1</td>
+            </tr>
+            <tr>
+                <th id="1691098179R19" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">20</div>
+                </th>
+                <td>Maaike</td>
+                <td>Gerlsma</td>
+                <td>altviool 2</td>
+            </tr>
+            <tr>
+                <th id="1691098179R20" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">21</div>
+                </th>
+                <td>Lia</td>
+                <td>van Schaijk</td>
+                <td>altviool 2</td>
+            </tr>
+            <tr>
+                <th id="1691098179R21" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">22</div>
+                </th>
+                <td>Laetitia </td>
+                <td>IngenHousz</td>
+                <td class="s2 softmerge" dir="ltr">
+                    <div class="softmerge-inner" style="width:133px;left:-1px">altviool 2 (aanvoerder)</div>
+                </td>
+            </tr>
+            <tr>
+                <th id="1691098179R22" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">23</div>
+                </th>
+                <td>Gill</td>
+                <td>Griffiths</td>
+                <td>cello</td>
+            </tr>
+            <tr>
+                <th id="1691098179R23" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">24</div>
+                </th>
+                <td>Paul</td>
+                <td>Janse</td>
+                <td>cello</td>
+            </tr>
+            <tr>
+                <th id="1691098179R24" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">25</div>
+                </th>
+                <td>Anke</td>
+                <td>Meester</td>
+                <td class="s2 softmerge" dir="ltr">
+                    <div class="softmerge-inner" style="width:110px;left:-1px">cello (aanvoerder)</div>
+                </td>
+            </tr>
+            <tr>
+                <th id="1691098179R25" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">26</div>
+                </th>
+                <td>Franca</td>
+                <td>Post</td>
+                <td>cello</td>
+            </tr>
+            <tr>
+                <th id="1691098179R26" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">27</div>
+                </th>
+                <td>Yo</td>
+                <td>van Dijk</td>
+                <td>contrabas</td>
+            </tr>
+            <tr>
+                <th id="1691098179R27" style="height: 20px;" class="row-headers-background">
+                    <div class="row-header-wrapper" style="line-height: 20px">28</div>
+                </th>
+                <td>Richard </td>
+                <td>Both</td>
+                <td>contrabas</td>
+            </tr>
+        </tbody>
+    </table>
+        </div>
+
+            <div class="clear";">
             <h4>Partijen (nog niet betekend)</h4>
             <ul style="column-count: 3;">
                 <li>
