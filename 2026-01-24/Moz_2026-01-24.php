785795261cdd608b1c010cafee0a563d3b9cd10a
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-06-17 20:41:08 +0200

    Add initial content for Pianoconcert nr. 20 in A KV 488 on 24 January 2026

---
+++ 2026-01-24/Moz_2026-01-24.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mozart op Zaterdag op 25 oktober 2025</title>
+    <link href="/css/moz.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div class="w3-content w3-white w3-panel">
+        <?php include_once 'navigatie.htm'; ?>
+        <h3>24 januari 2026:</h3>
+        <h2>Pianoconcert nr. 20 in A KV 488</h2>
+        <p>
+            De zevende aflevering van
+            <i>Mozart op Zaterdag</i>
+            vindt plaats op zaterdagochtend 24 januari. We werken dan aan Mozarts Pianoconcert nr. 20 in A KV 488. Het orkest bestaat uit een fluit, twee hobo's, twee fagotten, hoorns en strijkers. Het pianoconcert bestaat uit drie delen, een Allegro, een Adagio en een Allegro assai, en duurt ongeveer 30 minuten. Drie pianisten verdelen de delen.
+        </p>
+
+            <h4>De pianosolisten</h4>
+            <div class="w3-panel" style="float: left;">
+                <h5>Annette Middelbeek</h5>
+                <p>Annette Middelbeek begon al vroeg met pianospelen. Haar moeder gaf haar de eerste lessen die de aanzet vormden tot een professionele opleiding. Achtereenvolgens studeerde Annette in Utrecht, Parijs, Amsterdam en Londen bij respectievelijk Ria Groot, France Clidat, Willem Brons en John Bingham. In Parijs won zij in een concours een eerste prijs met maar liefst twee onderscheidingen. In diezelfde tijd voltooide zij aan de Universiteit van Utrecht een studie Muziekwetenschappen. Annette gaf vele soloconcerten en maakte diverse radio- en CD-opnames, waaronder de complete pianowerken van Leo Michielsen en Arthur Honegger. Voorts speelt Annette kamermuziek en treedt zij op als solist met orkest.</p>
+            </div>
+            <div class="w3-panel" style="float: left;">
+                <h5>Brit van Manen</h5>
+                <p>Brit sloot hoofdvak piano af bij Herman Uhlhorn aan het Utrechts Conservatorium. Daar behaalde zij ook de graad ensembleleiding. Bij Willem Brons perfectioneert zij haar pianospel en treedt met regelmaat op tijdens gezamenlijke projecten van het Scarbo pianistencollectief. Onlangs speelde zij met Arthur Bouma, een collega uit het pianistencollectief, live op Concertzender enkele préludes van Debussy als aankondiging van ons Debussy project op 15 juni a.s. in de vioolkist in Hilversum.
+Ook is zij als solist en/of begeleider van (kinder-) koren en kamermuziek te horen op lokale podia in Apeldoorn, Soest, Almere en Amersfoort. Zij geeft lessen als docent piano en bereidt studenten voor op het toelatingsexamen voor het Conservatorium. Aan het Conservatorium van Artez Zwolle behaalde zij het diploma docent muziek en heeft een eigen bedrijf in muziekeducatie. Zij geeft lessen als  docent muziek in het primair en voortgezet onderwijs en traint groepsleerkrachten om het vak muziek binnen het basisonderwijs vorm en inhoud te kunnen geven.</p>
+            </div>
+            <div class="w3-panel" style="float: left;">
+                <h5>Yumi Toyama</h5>
+                <p>Yumi Toyama, geboren in Japan, begon op 3-jarige leeftijd piano spelen. Ze heeft haar Bachelor of Music gehaald aan het Peabody Conservatory in Baltimore (VS) en haar Master Degree aan de Chopin Academy of Music in Warschau (Polen), waar ze na haar studie als pianiste werkte. Ze won diverse prijzen op Internationale concoursen, o.a. de Chopin-Prijs op Radziwił in Polen. Yumi woont al lang in Nederland en heeft meerdere jaren als pianiste bij The International Holland Music Sessions  (o.a. met H. Krebbers) gewerkt. Zij speelt als solopianiste en in kamermuziekverband, daarnaast werkt ze als docente. Diverse leerlingen van haar hebben concoursen gewonnen (zoals het Prinses Christina Concours).</p>
+            </div>
+
+        <p style="clear:both;">De partijen zijn binnenkort beschikbaar</p>
+        <div class="onzichtbaar" style="clear: both;">
+            <h4>Partijen (nog niet betekend)</h4>
+            <ul style="column-count: 3;">
+                <li>
+                    <a href="/2025-09-27/Mozart_Concertante_-_Oboe_1.pdf" target="_blank">Hobo 1</a>
+                </li>
+                <li>
+                    <a href="/2025-09-27/Mozart_Concertante_-_Oboe_2.pdf" target="_blank">Hobo 2</a>
+                </li>
+                <li>
+                    <a href="/2025-09-27/Mozart_Concertante_-_Horn_1_in_Eb.pdf" target="_blank">Hoorn 1 in Eb</a>
+                </li>
+                <li>
+                    <a href="/2025-09-27/Mozart_Concertante_-_Horn_1_in_Eb.pdf" target="_blank">Hoorn 2 in Eb</a>
+                </li>
+                <li>
+                    <a href="/2025-09-27/Mozart_Concertante_-_Horn_1_in_F.pdf" target="_blank">Hoorn 1 in F</a>
+                </li>
+                <li>
+                    <a href="/2025-09-27/Mozart_Concertante_-_Horn_1_in_F.pdf" target="_blank">Hoorn 2 in F</a>
+                </li>
+                <li>
+                    <a href="/2025-09-27/Mozart_Concertante_-_Violin_1.pdf" target="_blank">Viool 1</a>
+                </li>
+                <li>
+                    <a href="/2025-09-27/Mozart_Concertante_-_Violin_2.pdf" target="_blank">Viool 2</a>
+                </li>
+                <li>
+                    <a href="/2025-09-27/Mozart_Concertante_-_Viola_1.pdf" target="_blank">Altviool 1</a>
+                </li>
+                <li>
+                    <a href="/2025-09-27/Mozart_Concertante_-_Viola_2.pdf" target="_blank">Altviool 2</a>
+                </li>
+                <li>
+                    <a href="/2025-09-27/Mozart_Concertante_-_Cello.pdf" target="_blank">Cello</a>
+                </li>
+                <li>
+                    <a href="/2025-09-27/Mozart_Concertante_-_Double_Bass.pdf" target="_blank">Contrabas, fagot</a>
+                </li>
+                <li>
+                    <a href="/2025-09-27/Mozart_Concertante_-_partituur_NMA.pdf" target="_blank">
+                        <b>nog niet betekende partituur</b>
+                    </a>
+                </li>
+            </ul>
+            <p>
+                NB.
+                <b>
+                    Strijkers, neem SVP de streken uit de betekende partituur over in je eigen partij.
+            </p>
+        </div>
+    </div>
+</body>
+
+</html>
