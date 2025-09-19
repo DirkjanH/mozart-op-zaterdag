0af72cb998788ea802d468e91e8d4401182ff30e
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-09-19 20:47:03 +0200

    zsfgn b

--- session_destroy.php
+++ session_destroy.php
@@ -1,4 +1,14 @@
 <?php
+// stel php in dat deze fouten weergeeft
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once $_SERVER["DOCUMENT_ROOT"] . '/vendor/autoload.php';
+
+use function PHP81_BC\strftime;
+
+Kint::$enabled_mode = false; //($_SERVER['REMOTE_ADDR'] === '83.87.12.163');
+
 session_start();
 print_r($_SESSION);
 session_destroy();
