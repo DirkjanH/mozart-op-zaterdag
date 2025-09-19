a7f7672ea207732290c00d85a39090355ca94c16
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-09-19 20:44:39 +0200

    vbn

--- session_destroy.php
+++ session_destroy.php
@@ -1,4 +1,6 @@
 <?php
 session_destroy();
-?>
-
+echo "Session destroyed. You have been logged out.";
+header("Location: " . $_SERVER['PHP_SELF']);
+// Redirect to the login page or home page after logout
+exit;
