d9c8be9fd768b11905af71466b1260a20ee35259
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-09-22 18:43:06 +0200

    xvbhg n

--- test_inloggen.php
+++ test_inloggen.php
@@ -1,1 +1,11 @@
 <?php require_once '\includes\inloggen.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../w3.css">
+    <title>Mozart op Zaterdag - 25 oktober 2025</title>
