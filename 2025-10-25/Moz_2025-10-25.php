10818eec3afab34c926a0656575434d50f580c6e
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-09-22 18:34:03 +0200

    fgn

--- 2025-10-25/Moz_2025-10-25.php
+++ 2025-10-25/Moz_2025-10-25.php
@@ -1,3 +1,5 @@
+<?php require_once '\includes\inloggen.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,7 +14,7 @@
     <div class="w3-content w3-white w3-panel">
         <?php require_once '../navigatie.htm';
         echo 'Navigatie geladen<br>';
-        require_once '\includes\inloggen.php'; ?>
+        ?>
         <h3>25 oktober 2025:</h3>
         <h2>'Haffner' symfonie nr. 35 in D KV 385</h2>
         <p>
