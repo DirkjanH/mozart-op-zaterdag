3ac1a26c2dcc3de5f6c43051dda9dc93913e8d12
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-09-23 16:28:45 +0200

    f

--- 2025-09-27/Moz_2025-09-27.php
+++ 2025-09-27/Moz_2025-09-27.php
@@ -10,8 +10,8 @@
     <link href="/css/moz.css" rel="stylesheet" type="text/css">
 </head>
 
-<body>
-    <div class="w3-text-content w3-white w3-panel">
+<body class="w3-grey">
+    <div class="w3-content w3-white w3-panel">
         <?php include_once '../navigatie.htm'; ?>
         <h3>27 september 2025:</h3>
         <h2>Sinfonia concertante in Eb KV 364</h2>
