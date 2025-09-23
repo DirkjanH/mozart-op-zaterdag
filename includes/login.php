670d290bbd934df001f9446b8d3f47ab24a23117
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-09-23 16:18:53 +0200

    s

--- includes/login.php
+++ includes/login.php
@@ -23,7 +23,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             header('Location: ' . $redirect_url);
             exit;
         } else {
-            header('Location: /includes/index.php');
+            header('Location: /index.php');
         }
     } else $foutmelding = 'Ongeldige gebruikersnaam of wachtwoord.';
 }
