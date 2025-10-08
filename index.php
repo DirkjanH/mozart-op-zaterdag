7d198b62041464b3194b4f270752f1655049f584
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-10-08 18:45:48 +0200

    b

--- index.php
+++ index.php
@@ -34,79 +34,62 @@
         <h4 class="clear">Voor de komende afleveringen staan deze stukken op het programma:</h4>
         <ul class="programma">
             <li>
-                <p><b>27 september</b> in de
-                    <a href="/marnixzaal.php" target="_blank">Marnixzaal</a>: Sinfonia Concertante in Eb KV 364 voor 2
-                    hobo's, 1 fagot, 2 hoorns en strijkers. De bezetting is compleet.
+                <p><b>25 oktober</b> in de
+                    <a href="/marnixzaal.php" target="_blank">Marnixzaal</a>: de 'Haffner' symfonie nr. 35 in D KV 385
+                    voor 2 fluiten, 2 hobo's, 2 klarinetten, 2 fagotten, 2 hoorns, 2 trompetten, pauken en strijkers. De bezetting is bijna compleet. We hebben nu 30 mensen kunnen plaatsen, een mengeling van goede amateurs en professionals. Er is nog plaats voor een trompet, een 2e viool en een altviool.
                 </p>
-                <button class="toggle-knop" onclick="toonVerbergTekst('sept2025')">Lees verder</button>
-                <div id="sept2025" class="verborgen-tekst">
-                    <p> De Sinfonia concertante is een van de meest geliefde werken van Mozart. Het orkest bestaat uit
-                        twee hobo's,
-                        twee hoorns, fagot en strijkers, met gedeelde altviolen. De sinfonia bestaat uit drie delen, een Allegro maestoso,
-                    een Andante en een
-                    Presto, en duurt ongeveer 30 minuten. Het werk is een prachtig
-                        voorbeeld van muzikale interactie tussen de solisten en het orkest. De solisten zijn Valentina
-                        Bernardone
-                        (viool) en Anna Smith (altviool)
-                    </p>
-    </div>
-    </li>
-    <li>
-        <p><b>25 oktober</b> in de
-            <a href="/marnixzaal.php" target="_blank">Marnixzaal</a>: de 'Haffner' symfonie nr. 35 in D KV 385
-            voor 2 fluiten, 2 hobo's, 2 klarinetten, 2 fagotten, 2 hoorns, 2 trompetten, pauken en strijkers. De bezetting is bijna compleet. We hebben nu 30 mensen kunnen plaatsen, een mengeling van goede amateurs en professionals. Er is nog plaats voor een trompet, een 2e viool en een altviool.</p>
-        <button class="toggle-knop" onclick="toonVerbergTekst('okt2025')">Lees verder</button>
-            <div id="okt2025" class="verborgen-tekst">
-                <h4>De Oorsprong van de 'Haffner'</h4>
-                <p>De symfonie dankt zijn naam aan de familie Haffner, een prominente familie uit Salzburg waar
-                    Mozart bevriend mee was. Oorspronkelijk componeerde Mozart het werk in 1782 als een serenade ter gelegenheid van
-                    de verheffing in de adelstand van Siegmund Haffner. Later bewerkte Mozart de serenade tot een symfonie door onder
-                    andere een mars en twee menuetten te verwijderen en een nieuw openingsdeel toe te voegen, waardoor het de
-                    vorm kreeg die we nu kennen. De 'Haffner' symfonie is een prachtig voorbeeld van Mozarts meesterschap en zijn
-                    vermogen om zowel feestelijke grandeur als diepgaande emotie in zijn muziek te leggen. De
-            symfonie bestaat uit vier delen, een Allegro con spirito, een Andante, een Menuetto en Trio en een
-            Presto, en duurt ongeveer 20 minuten.</p>
-            </div>
-    </li>
-    <li>
-        <b>15 november</b> in de
-        <a href="/marnixzaal.php" target="_blank">Marnixzaal</a>: Pianoconcert nr. 17 in G KV 453 voor 1 fluit,
-        2 hobo's, 2 fagotten, 2 hoorns en strijkers. Solist: Hans-Erik Dijkstra (piano). <a
-            href="/2025-11-15/Moz_2025-11-15.php" target="_blank">Meer info & partijen vind je hier</a>.
-    </li>
-    <li>
-        <b>24 januari</b> in de
-        <a href="/marnixzaal.php" target="_blank">Marnixzaal</a>: Pianoconcert nr. 23 in A KV 488 voor 1 fluit,
-        2 klarinetten, 2 fagotten, 2 hoorns en strijkers. De solisten Annette Middelbeek, Brit van Manen en Yumi
-        Toyama spelen ieder een deel. <a href="/2026-01-24/Moz_2026-01-24.php" target="_blank">Meer info &
-            partijen vind je hier</a>.
-    </li>
-    </ul>
-    <p>Aan de programma's voor de volgende afleveringen in 2026 wordt nog gewerkt. Waarschijnlijk gaan er dan naast
-        symfonieën ook weer (solo)concerten van Mozart.</p>
-    <h4>Meespelen?</h4>
-    <p>Heb je belangstelling om eens mee te doen met Mozart op Zaterdag? Het is mogelijk je op te geven voor de
-        volgende afleveringen, die steeds plaatsvinden op de vierde zaterdag van de maand. In 2026 zijn dat dus 24
-        januari, 28 februari, 28 maart, 25 april en 23 mei, en daarna weer op 26 september, 24 oktober
-        en 21 november. We werken in de
-        <a href="/marnixzaal.php" target="_blank">Marnixzaal</a> van de vroegere Utrechtse Muziekschool aan het
-        Domplein of in het
-        <a href="/stadsklooster.php" target="_blank">Stadsklooster</a> in de wijk Lombok. Er wordt koffie en thee
-        geschonken in de zaal.
-    </p>
-    <h4>Tijden</h4>
-    <p>We werken als volgt: binnenkomen om 9:45, repeteren van 10:00 tot 12:30 met een korte pauze. Om 13:00 sluiten
-        we af met een concertje van 30-45 minuten.
-    </p>
-    <h4>Meld je aan met het formulier</h4>
-    <p>We hopen dat je deze geweldige muziek met ons wilt spelen! We formeren een bezetting van serieuze en ervaren
-        spelers die zich natuurlijk goed voorbereiden. We leveren op tijd (betekende) partijen als PDF's aan.
-        <a href="https://forms.gle/49YfM2dSn8AYpzfE8" target="_blank">In dit formulier kun je aangeven of en wanneer
-            je wilt meedoen</a>. Mogelijk zijn er meer inschrijvingen dan plaatsen.
-        <b>Reageer daarom snel!</b> Deelname is gratis; wel worden de deelnemers aan het eind om een vrijwillige
-        bijdrage in de kosten van de zaal e.d. gevraagd. T.z.t. hopen we de professionele deelnemers ook wat te
-        kunnen betalen.
-    </p>
+                <button class="toggle-knop" onclick="toonVerbergTekst('okt2025')">Lees verder</button>
+                <div id="okt2025" class="verborgen-tekst">
+                    <h4>De Oorsprong van de 'Haffner'</h4>
+                    <p>De symfonie dankt zijn naam aan de familie Haffner, een prominente familie uit Salzburg waar
+                        Mozart bevriend mee was. Oorspronkelijk componeerde Mozart het werk in 1782 als een serenade ter gelegenheid van
+                        de verheffing in de adelstand van Siegmund Haffner. Later bewerkte Mozart de serenade tot een symfonie door onder
+                        andere een mars en twee menuetten te verwijderen en een nieuw openingsdeel toe te voegen, waardoor het de
+                        vorm kreeg die we nu kennen. De 'Haffner' symfonie is een prachtig voorbeeld van Mozarts meesterschap en zijn
+                        vermogen om zowel feestelijke grandeur als diepgaande emotie in zijn muziek te leggen. De
+                        symfonie bestaat uit vier delen, een Allegro con spirito, een Andante, een Menuetto en Trio en een
+                        Presto, en duurt ongeveer 20 minuten.</p>
+                </div>
+            </li>
+            <li>
+                <b>15 november</b> in de
+                <a href="/marnixzaal.php" target="_blank">Marnixzaal</a>: Pianoconcert nr. 17 in G KV 453 voor 1 fluit,
+                2 hobo's, 2 fagotten, 2 hoorns en strijkers. Solist: Hans-Erik Dijkstra (piano). <a
+                    href="/2025-11-15/Moz_2025-11-15.php" target="_blank">Meer info & partijen vind je hier</a>.
+            </li>
+            <li>
+                <b>24 januari</b> in de
+                <a href="/marnixzaal.php" target="_blank">Marnixzaal</a>: Pianoconcert nr. 23 in A KV 488 voor 1 fluit,
+                2 klarinetten, 2 fagotten, 2 hoorns en strijkers. De solisten Annette Middelbeek, Brit van Manen en Yumi
+                Toyama spelen ieder een deel. <a href="/2026-01-24/Moz_2026-01-24.php" target="_blank">Meer info &
+                    partijen vind je hier</a>.
+            </li>
+        </ul>
+        <p>Aan de programma's voor de volgende afleveringen in 2026 wordt nog gewerkt. Waarschijnlijk gaan er dan naast
+            symfonieën ook weer (solo)concerten van Mozart.</p>
+        <h4>Meespelen?</h4>
+        <p>Heb je belangstelling om eens mee te doen met Mozart op Zaterdag? Het is mogelijk je op te geven voor de
+            volgende afleveringen, die steeds plaatsvinden op de vierde zaterdag van de maand. In 2026 zijn dat dus 24
+            januari, 28 februari, 28 maart, 25 april en 23 mei, en daarna weer op 26 september, 24 oktober
+            en 21 november. We werken in de
+            <a href="/marnixzaal.php" target="_blank">Marnixzaal</a> van de vroegere Utrechtse Muziekschool aan het
+            Domplein of in het
+            <a href="/stadsklooster.php" target="_blank">Stadsklooster</a> in de wijk Lombok. Er wordt koffie en thee
+            geschonken in de zaal.
+        </p>
+        <h4>Tijden</h4>
+        <p>We werken als volgt: binnenkomen om 9:45, repeteren van 10:00 tot 12:30 met een korte pauze. Om 13:00 sluiten
+            we af met een concertje van 30-45 minuten.
+        </p>
+        <h4>Meld je aan met het formulier</h4>
+        <p>We hopen dat je deze geweldige muziek met ons wilt spelen! We formeren een bezetting van serieuze en ervaren
+            spelers die zich natuurlijk goed voorbereiden. We leveren op tijd (betekende) partijen als PDF's aan.
+            <a href="https://forms.gle/49YfM2dSn8AYpzfE8" target="_blank">In dit formulier kun je aangeven of en wanneer
+                je wilt meedoen</a>. Mogelijk zijn er meer inschrijvingen dan plaatsen.
+            <b>Reageer daarom snel!</b> Deelname is gratis; wel worden de deelnemers aan het eind om een vrijwillige
+            bijdrage in de kosten van de zaal e.d. gevraagd. T.z.t. hopen we de professionele deelnemers ook wat te
+            kunnen betalen.
+        </p>
 
     </div>
 </body>
