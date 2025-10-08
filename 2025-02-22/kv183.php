28073c6734a1821c8923ae7797ef96e685690ea1
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-10-08 21:32:05 +0200

    b

--- 2025-02-22/kv183.php
+++ 2025-02-22/kv183.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Symfonie nr. 25 in g klein KV 183</title>
-    <link href="css/moz.css" rel="stylesheet" type="text/css">
+    <link href="/css/moz.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
