89ac04e4898fc7b1e19f0c4744f895fc61e05cd8
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-10-26 13:22:58 +0100

    m

--- vorige_afleveringen.php
+++ vorige_afleveringen.php
@@ -12,7 +12,7 @@
     <div class="w3-content w3-white w3-panel">
         <?php include_once 'navigatie.htm'; ?>
         <h2>Mozart op Zaterdag</h2>
-        <h4>22 februari 2025 - symfonie nr. 25 KV 183</h4>
+        <h4>22 februari 2025: Symfonie nr. 25 KV 183</h4>
         <p><img src="images/MoZ_foto_buigen_Yo.jpg" alt="Mozart"
                 class="w3-image w3-right w3-margin-left w3-margin-bottom" style="max-width: 500px;">De <a
                 href="\2025-02-22\kv183.php" target="_blank">eerste
@@ -22,7 +22,7 @@
             onder leiding van dirigent en initiatiefnemer <a href="https://www.horringa.net/index.php"
                 target="_blank">Dirkjan Horringa</a>. Een 50-tal mensen kwam luisteren en was enthousiast.</p>
         <div style="clear: both;" class="w3-panel">
-            <h4>26 april 2025 - Symfonie nr. 4 in D KV 19 en het klarinetconcert in A-groot KV 622</h4>
+            <h4>26 april 2025: Symfonie nr. 4 in D KV 19 en het klarinetconcert in A-groot KV 622</h4>
             <p><img src="/images/2025-04-26 Mozart op Zaterdag klarinetconcert.jpg" alt="Mozart klarinetconcert"
                     class="w3-image w3-left w3-margin-right w3-margin-bottom" style="max-width: 500px;">Op 26 april
                 (koningsdag) vond de <a href="2025-04-26\Moz_2025-04-26.php" target="_blank"></a>tweede
@@ -35,30 +35,34 @@
             </p>
         </div>
         <div style="clear: both;" class="w3-panel">
-        <h4>Vioolconcert nr. 4 in D KV 218</h4>
-        <p>
-            <img src="/2025-05-24/evelyn.jpg" alt="Evelyn Tjon-En-Fa" width="250"
-                class="w3-image w3-right w3-margin-left w3-margin-bottom geenlijn">De derde aflevering van <i>Mozart op Zaterdag</i>
-            vond plaats op zaterdagochtend 24 mei. We werkten aan Mozarts vierde vioolconcert in D KV 218. Mozart
-            schreef zijn vierde vioolconcert in D groot KV 218 in 1775 in Salzburg. Soliste was Evelyn Tjon-En-Fa. Zij werkte eerder mee als concertmeester aan Mozart op Zaterdag. In 2001 studeerde Evelyn cum laude af als uitvoerend musicus aan het conservatorium in Tilburg (Fontys Hogescholen). Naast haar
-            optredens als violiste werkt Evelyn sinds 2001 als advocaat.
-        </p>
+            <h4>24 mei 2025: Vioolconcert nr. 4 in D KV 218</h4>
+            <p>
+                <img src="/2025-05-24/evelyn.jpg" alt="Evelyn Tjon-En-Fa" width="250"
+                    class="w3-image w3-right w3-margin-left w3-margin-bottom geenlijn">De derde aflevering van <i>Mozart op Zaterdag</i>
+                vond plaats op zaterdagochtend 24 mei. We werkten aan Mozarts vierde vioolconcert in D KV 218. Mozart
+                schreef zijn vierde vioolconcert in D groot KV 218 in 1775 in Salzburg. Soliste was Evelyn Tjon-En-Fa. Zij werkte eerder mee als concertmeester aan Mozart op Zaterdag. In 2001 studeerde Evelyn cum laude af als uitvoerend musicus aan het conservatorium in Tilburg (Fontys Hogescholen). Naast haar
+                optredens als violiste werkt Evelyn sinds 2001 als advocaat.
+            </p>
         </div>
         <div style="clear: both;" class="w3-panel">
-        <h4>Sinfonia concertante in Eb KV 364</h4>
-        <p>
-            De vierde aflevering van <i>Mozart op Zaterdag</i> vond plaats op zaterdagochtend 27 september. We werkten toen aan Mozarts Sinfonia concertante in Es KV 364 voor viool, altviool en orkest. De sinfonia bestaat uit drie delen, een Allegro maestoso, een Andante en een Presto, en duurt ongeveer 30 minuten.
-De Sinfonia concertante is een van de meest geliefde werken van Mozart. Het orkest bestaat uit twee hobo's, twee hoorns, fagot en strijkers, met gedeelde altviolen. Het werk is een prachtig voorbeeld van muzikale interactie tussen de solisten en het orkest. De solisten waren Valentina Bernardone (viool) en Anna Smith (altviool).
-        <div style="clear: both; display: table; margin: auto;">
-            <div style="display: table-cell;" class="w3-small w3-center"><img
-                    src="/2025-09-27/valentina-bernardone-nederlands-.ed8ef09e.fill-1280x1280.jpg" width="250"
-                    alt="Valentina Bernardone" class="w3-image w3-panel w3-left geenlijn"><br>Valentina Bernardone,
-                viool
+            <h4>27 september 2025: Sinfonia concertante in Eb KV 364</h4>
+            <p>
+                De vierde aflevering van <i>Mozart op Zaterdag</i> vond plaats op zaterdagochtend 27 september. We werkten toen aan Mozarts Sinfonia concertante in Es KV 364 voor viool, altviool en orkest. De sinfonia bestaat uit drie delen, een Allegro maestoso, een Andante en een Presto, en duurt ongeveer 30 minuten.
+                De Sinfonia concertante is een van de meest geliefde werken van Mozart. Het orkest bestaat uit twee hobo's, twee hoorns, fagot en strijkers, met gedeelde altviolen. Het werk is een prachtig voorbeeld van muzikale interactie tussen de solisten en het orkest. De solisten waren Valentina Bernardone (viool) en Anna Smith (altviool).
+            <div style="clear: both; display: table; margin: auto;">
+                <div style="display: table-cell;" class="w3-small w3-center"><img
+                        src="/2025-09-27/valentina-bernardone-nederlands-.ed8ef09e.fill-1280x1280.jpg" width="250"
+                        alt="Valentina Bernardone" class="w3-image w3-panel w3-left geenlijn"><br>Valentina Bernardone,
+                    viool
+                </div>
+                <div style="display: table-cell;" class="w3-small w3-center"><img src="/2025-09-27/Anna-Smith-vierkant-.jpg"
+                        width="250" alt="Anna Smith" class="w3-image w3-panel w3-left geenlijn"><br>Anna Smith, altviool
+                </div>
             </div>
-            <div style="display: table-cell;" class="w3-small w3-center"><img src="/2025-09-27/Anna-Smith-vierkant-.jpg"
-                    width="250" alt="Anna Smith" class="w3-image w3-panel w3-left geenlijn"><br>Anna Smith, altviool
+            <div style="clear: both;" class="w3-panel">
+                <h4>25 oktober 2025: 'Haffner' symfonie nr. 35 in D KV 385</h4>
+                <p>De vijfde aflevering van Mozart op Zaterdag vond plaats op zaterdagochtend 25 oktober. We werkten toen aan Mozarts 'Haffner' symfonie nr. 35 in D KV 385 voor uitgebreid orkest met dubbel hout, hoorns, trompetten en pauken. De symfonie dankt zijn naam aan de familie Haffner, een prominente familie uit Salzburg waar Mozart bevriend mee was. Oorspronkelijk componeerde Mozart het werk in 1782 als een serenade ter gelegenheid van de verheffing in de adelstand van Siegmund Haffner. Later bewerkte Mozart de serenade tot een symfonie door onder andere een mars en twee menuetten te verwijderen en een nieuw openingsdeel toe te voegen, waardoor het de vorm kreeg die wij nu kennen. De 'Haffner' symfonie is een prachtig voorbeeld van Mozarts meesterschap en zijn vermogen om zowel feestelijke grandeur als diepgaande emotie in zijn muziek te leggen.</p>
             </div>
-        </div>
         </div>
     </div>
 </body>
