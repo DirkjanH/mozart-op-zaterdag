099f62e9571bff49db69d2ec9d34cae7af14086f
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-11-27 11:02:26 +0100

    k

--- 2026-01-24/Moz_2026-01-24.php
+++ 2026-01-24/Moz_2026-01-24.php
@@ -91,7 +91,7 @@
             </p>
         </div>
         <h2>Bezetting</h2>
-        <p>Er zijn nu 24 deelnemers. De blazers zijn compleet, maar er is nog plaats voor 1 tweede viool en 2 altviolen.</p>
+        <p>Er zijn nu 26 deelnemers. De blazers zijn compleet, maar er is nog plaats voor 1 tweede viool en 2 altviolen.</p>
         <p class="onzichtbaar">Binnenkort plaatsen we hier de lijst van alle deelnemers.</p>
         <table class="w3-table w3-striped w3-bordered" id="deelnemers">
             <thead>
