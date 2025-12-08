20bbad19830df5e5684fbbb77275b2746036606c
Dirkjan Horringa <dirkjan@pellegrina.net>
2025-12-08 16:25:22 +0100

    m

--- 2026-02-28/Moz_2026-02-28.php
+++ 2026-02-28/Moz_2026-02-28.php
@@ -1,3 +1,5 @@
+<?php require_once '../includes/inloggen.php'; ?>
+
 <!DOCTYPE html>
 <html lang="nl">
 
@@ -17,58 +19,219 @@
             De achtste aflevering van <i>Mozart op Zaterdag</i> vindt plaats op zaterdagochtend 28 februari. We werken dan aan Mozarts Symfonie nr. 40 in g klein KV 550. Het orkest bestaat uit een fluit, twee hobo's, twee klarinetten, twee fagotten, twee hoorns en strijkers. De symfonie bestaat uit vier delen, een Molto Allegro, een Andante, een Menuetto en Trio en een Allegro assai, en duurt ongeveer 27 minuten.
         </p>
 
-        <p style="clear:both;">De partijen zijn binnenkort beschikbaar</p>
-        <div class="onzichtbaar" style="clear: both;">
+        <p style="clear:both;" class="onzichtbaar">De partijen zijn binnenkort beschikbaar</p>
+        <div class="" style="clear: both;">
             <h4>Partijen (nog niet betekend)</h4>
             <ul style="column-count: 3;">
                 <li class="aflevering">
-                    <a href="/2025-09-27/Mozart_Concertante_-_Oboe_1.pdf" target="_blank">Hobo 1</a>
+                    <a href="/2026-02-28/Mozart--symph40-flt.pdf" target="_blank">Fluit</a>
                 </li>
                 <li class="aflevering">
-                    <a href="/2025-09-27/Mozart_Concertante_-_Oboe_2.pdf" target="_blank">Hobo 2</a>
+                    <a href="/2026-02-28/Mozart--symph40-oboes.pdf" target="_blank">Hobo 1-2</a>
                 </li>
                 <li class="aflevering">
-                    <a href="/2025-09-27/Mozart_Concertante_-_Horn_1_in_Eb.pdf" target="_blank">Hoorn 1 in Eb</a>
+                    <a href="/2026-02-28/Mozart--symph40-clarinets.pdf" target="_blank">Klarinet 1-2</a>
                 </li>
                 <li class="aflevering">
-                    <a href="/2025-09-27/Mozart_Concertante_-_Horn_1_in_Eb.pdf" target="_blank">Hoorn 2 in Eb</a>
+                    <a href="/2026-02-28/Mozart--symph40-bassoons.pdf" target="_blank">Fagot 1-2</a>
                 </li>
                 <li class="aflevering">
-                    <a href="/2025-09-27/Mozart_Concertante_-_Horn_1_in_F.pdf" target="_blank">Hoorn 1 in F</a>
+                    <a href="/2026-02-28/Mozart--symph40-horns.pdf" target="_blank">Hoorn 1-2 in Bb alto</a>
                 </li>
                 <li class="aflevering">
-                    <a href="/2025-09-27/Mozart_Concertante_-_Horn_1_in_F.pdf" target="_blank">Hoorn 2 in F</a>
+                    <a href="/2026-02-28/Mozart--symph40-vln1.pdf" target="_blank">Viool 1</a>
                 </li>
                 <li class="aflevering">
-                    <a href="/2025-09-27/Mozart_Concertante_-_Violin_1.pdf" target="_blank">Viool 1</a>
+                    <a href="/2026-02-28/Mozart--symph40-vln2.pdf" target="_blank">Viool 2</a>
                 </li>
                 <li class="aflevering">
-                    <a href="/2025-09-27/Mozart_Concertante_-_Violin_2.pdf" target="_blank">Viool 2</a>
+                    <a href="/2026-02-28/Mozart--symph40-vla.pdf" target="_blank">Altviool</a>
                 </li>
                 <li class="aflevering">
-                    <a href="/2025-09-27/Mozart_Concertante_-_Viola_1.pdf" target="_blank">Altviool 1</a>
+                    <a href="/2026-02-28/Mozart--symph40-cello.pdf" target="_blank">Cello</a>
                 </li>
                 <li class="aflevering">
-                    <a href="/2025-09-27/Mozart_Concertante_-_Viola_2.pdf" target="_blank">Altviool 2</a>
+                    <a href="/2026-02-28/Mozart--symph40-bass.pdf" target="_blank">Contrabas</a>
                 </li>
                 <li class="aflevering">
-                    <a href="/2025-09-27/Mozart_Concertante_-_Cello.pdf" target="_blank">Cello</a>
-                </li>
-                <li class="aflevering">
-                    <a href="/2025-09-27/Mozart_Concertante_-_Double_Bass.pdf" target="_blank">Contrabas, fagot</a>
-                </li>
-                <li class="aflevering">
-                    <a href="/2025-09-27/Mozart_Concertante_-_partituur_NMA.pdf" target="_blank">
-                        <b>nog niet betekende partituur</b>
+                    <a href="/2026-02-28/Mozart--symph40-score v2.pdf" target="_blank">
+                        <b>Partituur</b>
                     </a>
                 </li>
             </ul>
-            <p>
-                NB.
-                <b>
-                    Strijkers, neem SVP de streken uit de betekende partituur over in je eigen partij.
-            </p>
         </div>
+        <h2>Bezetting</h2>
+        <p>Er zijn nu 31 deelnemers. Er is uitsluitend nog plaats voor een contrabas.</p>
+        <p class="onzichtbaar">Binnenkort plaatsen we hier de lijst van alle deelnemers.</p>
+        <table class="w3-table w3-striped w3-bordered" id="deelnemers">
+            <thead>
+                <tr>
+                    <th>voornaam</th>
+                    <th>achternaam</th>
+                    <th>instrument</th>
+                </tr>
+            </thead>
+            <tr>
+                <td>Marieke</td>
+                <td>van Dantzig</td>
+                <td>fluit</td>
+            </tr>
+            <tr>
+                <td>Erik</td>
+                <td>van Kassel</td>
+                <td>hobo 1</td>
+            </tr>
+            <tr>
+                <td>Liesbeth</td>
+                <td>Feikema</td>
+                <td>hobo 2</td>
+            </tr>
+            <tr>
+                <td>Ella</td>
+                <td>Broekstra</td>
+                <td>klarinet 1</td>
+            </tr>
+            <tr>
+                <td>Zwaantje </td>
+                <td>Schijve </td>
+                <td>klarinet 2</td>
+            </tr>
+            <tr>
+                <td>Yolande </td>
+                <td>Krooshof</td>
+                <td>fagot 1</td>
+            </tr>
+            <tr>
+                <td>Boukje</td>
+                <td>Jonkheer</td>
+                <td>fagot 2</td>
+            </tr>
+            <tr>
+                <td>Wim</td>
+                <td>Kuppen</td>
+                <td>hoorn 1</td>
+            </tr>
+            <tr>
+                <td>Piet</td>
+                <td>van den Heuvel</td>
+                <td>hoorn 2</td>
+            </tr>
+            <tr>
+                <td>Carla</td>
+                <td>Van Kassel-Westhof</td>
+                <td>viool 1</td>
+            </tr>
+            <tr>
+                <td>Carolien</td>
+                <td>Rijkmans</td>
+                <td>viool 1</td>
+            </tr>
+            <tr>
+                <td>Daniela</td>
+                <td>Roeland</td>
+                <td>viool 1</td>
+            </tr>
+            <tr>
+                <td>Mirte</td>
+                <td>Hulscher</td>
+                <td>viool 1</td>
+            </tr>
+            <tr>
+                <td>Bea</td>
+                <td>Selman</td>
+                <td>viool 1</td>
+            </tr>
+            <tr>
+                <td>Corinne</td>
+                <td>Britzel</td>
+                <td>viool 1</td>
+            </tr>
+            <tr>
+                <td>Adriaan</td>
+                <td>Mol</td>
+                <td>viool 2</td>
+            </tr>
+            <tr>
+                <td>Tonnie</td>
+                <td>Mols</td>
+                <td>viool 2</td>
+            </tr>
+            <tr>
+                <td>Barbara</td>
+                <td>Hendricx</td>
+                <td>viool 2</td>
+            </tr>
+            <tr>
+                <td>Erika</td>
+                <td>Mathot</td>
+                <td>viool 2</td>
+            </tr>
+            <tr>
+                <td>Eleanor</td>
+                <td>Peverall</td>
+                <td>viool 2</td>
+            </tr>
+            <tr>
+                <td>Jan</td>
+                <td>Prins</td>
+                <td>viool 2</td>
+            </tr>
+            <tr>
+                <td>Saskia</td>
+                <td>Wieberdink</td>
+                <td>viool 2</td>
+            </tr>
+            <tr>
+                <td>Marijke</td>
+                <td>Huijgen</td>
+                <td>altviool</td>
+            </tr>
+            <tr>
+                <td>Maria</td>
+                <td>Voorhout</td>
+                <td>altviool</td>
+            </tr>
+            <tr>
+                <td>Peter</td>
+                <td>Klusener</td>
+                <td>altviool</td>
+            </tr>
+            <tr>
+                <td>Laetitia </td>
+                <td>IngenHousz</td>
+                <td>altviool</td>
+            </tr>
+            <tr>
+                <td>Friso</td>
+                <td>van der Veen</td>
+                <td>cello</td>
+            </tr>
+            <tr>
+                <td>Meike</td>
+                <td>Jongejan </td>
+                <td>cello</td>
+            </tr>
+            <tr>
+                <td>Robin</td>
+                <td>Gerretsen</td>
+                <td>cello</td>
+            </tr>
+            <tr>
+                <td>Jan</td>
+                <td>Strumphler</td>
+                <td>cello</td>
+            </tr>
+            <tr>
+                <td>Frederik</td>
+                <td>Advokaat</td>
+                <td>contrabas</td>
+            </tr>
+            <tr>
+                <td><i>vacature</i></td>
+                <td></td>
+                <td>contrabas</td>
+            </tr>
+        </table>
     </div>
 </body>
 
