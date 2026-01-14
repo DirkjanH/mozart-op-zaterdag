1a64b6b8bef1ddff0e291ee4733e3f765ff2fbac
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-01-14 18:03:23 +0100

    m

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
             </li>
             <li>
                 <b>23 mei:</b> Pianoconcert nr. 24 in c klein KV 491 voor 1 fluit,
-                2 hobo's, 2 klarinetten, 2 fagotten, 2 hoorns en strijkers. De solist is nog te bepalen. <a xxxx="/2026-02-28/Moz_2026-02-28.php" target="_blank">Meer info & partijen vind je binnenkort hier</a>.
+                2 hobo's, 2 klarinetten, 2 fagotten, 2 hoorns en strijkers. Solist is de pianist Hans-Erik Dijkstra. <a xxxx="/2026-02-28/Moz_2026-02-28.php" target="_blank">Meer info & partijen vind je binnenkort hier</a>.
             </li>
             <li>
                 <b>26 september:</b> Concert voor fluit en harp in C KV 417 voor 2 hobo's, 1 fagot, 2 hoorns en strijkers. <a xxxx="/2026-02-28/Moz_2026-02-28.php" target="_blank">Meer info & partijen vind je binnenkort hier</a>.
