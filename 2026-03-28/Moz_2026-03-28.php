9f991477133351407ca4e5a6f4cce6ca32a3abdb
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-01-26 15:01:05 +0100

    m

--- 2026-03-28/Moz_2026-03-28.php
+++ 2026-03-28/Moz_2026-03-28.php
@@ -19,7 +19,7 @@
 
         <p style="clear:both;" class="onzichtbaar">De partijen zijn binnenkort beschikbaar</p>
         <div style="clear: both;">
-            <h4>Partijen (nog niet betekend)</h4>
+            <h3>Partijen (nog niet betekend)</h3>
             <h5>Symfonie nr. 13 in F KV 112</h5>
             <ul style="column-count: 3;">
                 <li class="aflevering">
