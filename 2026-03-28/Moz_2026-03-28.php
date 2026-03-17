3f624ccb5b3733f6179de6b02ce06dd33c0c34c9
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-03-17 14:29:23 +0100

    m

--- 2026-03-28/Moz_2026-03-28.php
+++ 2026-03-28/Moz_2026-03-28.php
@@ -21,240 +21,237 @@
         <p>Hou bij het voorbereiden rekening met deze tempi:</p>
         <p><b>Symfonie nr. 13 KV 112:</b></p>
         <ol>
-            <li >
-                <em >Allegro</em>: 132 voor de kwart
+            <li>
+                <em>Allegro</em>: 132 voor de kwart
             </li>
-            <li >
-                <em >Andante</em>: 44 voor de kwart of 88 voor de achtste
+            <li>
+                <em>Andante</em>: 44 voor de kwart of 88 voor de achtste
             </li>
-            <li >
+            <li>
                 <font face="tahoma, sans-serif"><em style="box-sizing:in=herit">Menuetto en Trio</em>
             </li>
             <ol start="a" style="box-sizing=:inherit;list-style-type:lower-latin">
-                <li >
+                <li>
                     <i>Menuetto</i>: 50 voor de gepuncteerde halve noot
                 </li>
-                <li >
+                <li>
                     <i>Trio</i>: 43 voor de gepuncteerde halve noot
                 </li>
             </ol>
-            <li >
+            <li>
                 <i>Molto Allegro</i>: 80 voor de gepuncteerde halve noot
             </li>
         </ol>
         <p><b>Hoornconcert nr. 2 in Es KV 417:</b></p>
-    </div>
-    <div>
         <ol>
-            <li >
-                <><em >Allegro</em> 120 voor de kwart
+            <li>
+                <><em>Allegro</em> 120 voor de kwart
             </li>
-            <li >
+            <li>
                 <em>Andante</em>: 88 voor de achtste
             </li>
             <li>
-                <em >Rondo - Allegro</em>: 100 voor de gepuncteerde halve noot
+                <em>Rondo - Allegro</em>: 100 voor de gepuncteerde halve noot
             </li>
         </ol>
-    </div>
 
-    <p style="clear:both;" class="onzichtbaar">De partijen zijn binnenkort beschikbaar</p>
-    <div style="clear: both;">
-        <h3>Partijen</h3>
-        <h5>Symfonie nr. 13 in F KV 112</h5>
-        <ul style="column-count: 3;">
-            <li class="aflevering">
-                <a href="/2026-03-28/KV112 - Oboe.pdf" target="_blank">Hobo 1/2</a>
-            </li>
-            <li class="aflevering">
-                <a href="/2026-03-28/KV112 - Trompa_en_Fa.pdf" target="_blank">Hoorn 1/2</a>
-            </li>
-            <li class="aflevering">
-                <a href="/2026-03-28/KV112 - Violin I - betekend.pdf" target="_blank"><b>Viool 1 (betekend)</b></a>
-            </li>
-            <li class="aflevering">
-                <a href="/2026-03-28/KV112 - Violin II - betekend.pdf" target="_blank"><b>Viool 2 (betekend)</b></a>
-            </li>
-            <li class="aflevering">
-                <a href="/2026-03-28/KV112 - Viola - betekend.pdf" target="_blank"><b>Altviool (betekend)</b></a>
-            </li>
-            <li class="aflevering">
-                <a href="/2026-03-28/KV112 - Cello - betekend.pdf" target="_blank"><b>Cello, contrabas, fagot (betekend)</b></a>
-            </li>
-            <li class="aflevering">
-                <a href="/2026-03-28/Mozart - partituur NMA KV112.pdf" target="_blank">
-                    Partituur
-                </a>
-            </li>
-        </ul>
-        <h5>Hoornconcert nr. 2 in Es KV 417</h5>
-        <ul style="column-count: 3;">
-            <li class="aflevering">
-                <a href="/2026-03-28/KV417 - Oboe_I.pdf" target="_blank">Hobo 1</a>
-            </li>
-            <li class="aflevering">
-                <a href="/2026-03-28/KV417 - Oboe_II.pdf" target="_blank">Hobo 2</a>
-            </li>
-            <li class="aflevering">
-                <a href="/2026-03-28/KV417 - Corno_I_en_Eb.pdf" target="_blank">Hoorn 1</a>
-            </li>
-            <li class="aflevering">
-                <a href="/2026-03-28/KV417 - Corno_II_en_Eb.pdf" target="_blank">Hoorn 2</a>
-            </li>
-            <li class="aflevering">
-                <a href="/2026-03-28/KV417 - Violin I - betekend.pdf" target="_blank"><b>Viool 1 (betekend)</b></a>
-            </li>
-            <li class="aflevering">
-                <a href="/2026-03-28/KV417 - Violin II - betekend.pdf" target="_blank"><b>Viool 2 (betekend)</b></a>
-            </li>
-            <li class="aflevering">
-                <a href="/2026-03-28/KV417 - viola - betekend.pdf" target="_blank"><b>Altviool (betekend)</b></a>
-            </li>
-            <li class="aflevering">
-                <a href="/2026-03-28/KV417 - Cello - betekend.pdf" target="_blank"><b>Cello, contrabas, fagot (betekend)</b></a>
-            </li>
-            <li class="aflevering">
-                <a href="/2026-03-28/Mozart - partituur NMA KV417.pdf" target="_blank">
-                    Partituur
-                </a>
-            </li>
-        </ul>
-    </div>
-    <h2>Bezetting</h2>
-    <p>Er zijn nu 24 deelnemers. De bezetting is compleet.</p>
-    <p class="onzichtbaar">Binnenkort plaatsen we hier de lijst van alle deelnemers.</p>
-    <table class="w3-table w3-striped w3-bordered" id="deelnemers">
-        <thead>
-            <tr>
-                <th>voornaam</th>
-                <th>achternaam</th>
-                <th>instrument</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Erik</td>
-                <td>van Kassel</td>
-                <td>hobo 1</td>
-            </tr>
-            <tr>
-                <td>Rineke</td>
-                <td>Boerwinkel</td>
-                <td>hobo 2</td>
-            </tr>
-            <tr>
-                <td>Yolande </td>
-                <td>Krooshof</td>
-                <td>fagot</td>
-            </tr>
-            <tr>
-                <td>Wim</td>
-                <td>Kuppen</td>
-                <td>hoorn 1</td>
-            </tr>
-            <tr>
-                <td>Irene</td>
-                <td>de Graauw</td>
-                <td>hoorn 2</td>
-            </tr>
-            <tr>
-                <td>Carla</td>
-                <td>Van Kassel-Westhof</td>
-                <td>viool 1 (concertmeester)</td>
-            </tr>
-            <tr>
-                <td>Corinne</td>
-                <td>Britzel</td>
-                <td>viool 1</td>
-            </tr>
-            <tr>
-                <td>Ruben</td>
-                <td>Faas</td>
-                <td>viool 1</td>
-            </tr>
-            <tr>
-                <td>Sophie</td>
-                <td>Vermond</td>
-                <td>viool 1</td>
-            </tr>
-            <tr>
-                <td>Vera</td>
-                <td>Heins</td>
-                <td>viool 1</td>
-            </tr>
-            <tr>
-                <td><i>vacature</i></td>
-                <td></td>
-                <td>viool 2 (aanvoerder)</td>
-            </tr>
-            <tr>
-                <td>Adriaan</td>
-                <td>Mol</td>
-                <td>viool 2</td>
-            </tr>
-            <tr>
-                <td>Bea</td>
-                <td>Selman</td>
-                <td>viool 2</td>
-            </tr>
-            <tr>
-                <td>Daniela</td>
-                <td>Roeland</td>
-                <td>viool 2</td>
-            </tr>
-            <tr>
-                <td>Jan</td>
-                <td>Prins</td>
-                <td>viool 2</td>
-            </tr>
-            <tr>
-                <td>Eline</td>
-                <td>Snoek</td>
-                <td>altviool (aanvoerder)</td>
-            </tr>
-            <tr>
-                <td>Marijke</td>
-                <td>Huijgen</td>
-                <td>altviool</td>
-            </tr>
-            <tr>
-                <td>Saskia</td>
-                <td>Wieberdink</td>
-                <td>altviool</td>
-            </tr>
-            <tr>
-                <td>Peter</td>
-                <td>Klusener</td>
-                <td>altviool</td>
-            </tr>
-            <tr>
-                <td>Gill</td>
-                <td>Griffiths</td>
-                <td>cello</td>
-            </tr>
-            <tr>
-                <td>Jan</td>
-                <td>Strumphler</td>
-                <td>cello</td>
-            </tr>
-            <tr>
-                <td>Robin</td>
-                <td>Gerretsen</td>
-                <td>cello</td>
-            </tr>
-            <tr>
-                <td>Anke</td>
-                <td>Meesters</td>
-                <td>cello (aanvoerder)</td>
-            </tr>
-            <tr>
-                <td>Ernő</td>
-                <td>Groeneweg</td>
-                <td>contrabas</td>
-            </tr>
-        </tbody>
-    </table>
-    <p>&nbsp;</p>
+        <p style="clear:both;" class="onzichtbaar">De partijen zijn binnenkort beschikbaar</p>
+        <div style="clear: both;">
+            <h3>Partijen</h3>
+            <h5>Symfonie nr. 13 in F KV 112</h5>
+            <ul style="column-count: 3;">
+                <li class="aflevering">
+                    <a href="/2026-03-28/KV112 - Oboe.pdf" target="_blank">Hobo 1/2</a>
+                </li>
+                <li class="aflevering">
+                    <a href="/2026-03-28/KV112 - Trompa_en_Fa.pdf" target="_blank">Hoorn 1/2</a>
+                </li>
+                <li class="aflevering">
+                    <a href="/2026-03-28/KV112 - Violin I - betekend.pdf" target="_blank"><b>Viool 1 (betekend)</b></a>
+                </li>
+                <li class="aflevering">
+                    <a href="/2026-03-28/KV112 - Violin II - betekend.pdf" target="_blank"><b>Viool 2 (betekend)</b></a>
+                </li>
+                <li class="aflevering">
+                    <a href="/2026-03-28/KV112 - Viola - betekend.pdf" target="_blank"><b>Altviool (betekend)</b></a>
+                </li>
+                <li class="aflevering">
+                    <a href="/2026-03-28/KV112 - Cello - betekend.pdf" target="_blank"><b>Cello, contrabas, fagot (betekend)</b></a>
+                </li>
+                <li class="aflevering">
+                    <a href="/2026-03-28/Mozart - partituur NMA KV112.pdf" target="_blank">
+                        Partituur
+                    </a>
+                </li>
+            </ul>
+            <h5>Hoornconcert nr. 2 in Es KV 417</h5>
+            <ul style="column-count: 3;">
+                <li class="aflevering">
+                    <a href="/2026-03-28/KV417 - Oboe_I.pdf" target="_blank">Hobo 1</a>
+                </li>
+                <li class="aflevering">
+                    <a href="/2026-03-28/KV417 - Oboe_II.pdf" target="_blank">Hobo 2</a>
+                </li>
+                <li class="aflevering">
+                    <a href="/2026-03-28/KV417 - Corno_I_en_Eb.pdf" target="_blank">Hoorn 1</a>
+                </li>
+                <li class="aflevering">
+                    <a href="/2026-03-28/KV417 - Corno_II_en_Eb.pdf" target="_blank">Hoorn 2</a>
+                </li>
+                <li class="aflevering">
+                    <a href="/2026-03-28/KV417 - Violin I - betekend.pdf" target="_blank"><b>Viool 1 (betekend)</b></a>
+                </li>
+                <li class="aflevering">
+                    <a href="/2026-03-28/KV417 - Violin II - betekend.pdf" target="_blank"><b>Viool 2 (betekend)</b></a>
+                </li>
+                <li class="aflevering">
+                    <a href="/2026-03-28/KV417 - viola - betekend.pdf" target="_blank"><b>Altviool (betekend)</b></a>
+                </li>
+                <li class="aflevering">
+                    <a href="/2026-03-28/KV417 - Cello - betekend.pdf" target="_blank"><b>Cello, contrabas, fagot (betekend)</b></a>
+                </li>
+                <li class="aflevering">
+                    <a href="/2026-03-28/Mozart - partituur NMA KV417.pdf" target="_blank">
+                        Partituur
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <h2>Bezetting</h2>
+        <p>Er zijn nu 24 deelnemers. De bezetting is compleet.</p>
+        <p class="onzichtbaar">Binnenkort plaatsen we hier de lijst van alle deelnemers.</p>
+        <table class="w3-table w3-striped w3-bordered" id="deelnemers">
+            <thead>
+                <tr>
+                    <th>voornaam</th>
+                    <th>achternaam</th>
+                    <th>instrument</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Erik</td>
+                    <td>van Kassel</td>
+                    <td>hobo 1</td>
+                </tr>
+                <tr>
+                    <td>Rineke</td>
+                    <td>Boerwinkel</td>
+                    <td>hobo 2</td>
+                </tr>
+                <tr>
+                    <td>Yolande </td>
+                    <td>Krooshof</td>
+                    <td>fagot</td>
+                </tr>
+                <tr>
+                    <td>Wim</td>
+                    <td>Kuppen</td>
+                    <td>hoorn 1</td>
+                </tr>
+                <tr>
+                    <td>Irene</td>
+                    <td>de Graauw</td>
+                    <td>hoorn 2</td>
+                </tr>
+                <tr>
+                    <td>Carla</td>
+                    <td>Van Kassel-Westhof</td>
+                    <td>viool 1 (concertmeester)</td>
+                </tr>
+                <tr>
+                    <td>Corinne</td>
+                    <td>Britzel</td>
+                    <td>viool 1</td>
+                </tr>
+                <tr>
+                    <td>Ruben</td>
+                    <td>Faas</td>
+                    <td>viool 1</td>
+                </tr>
+                <tr>
+                    <td>Sophie</td>
+                    <td>Vermond</td>
+                    <td>viool 1</td>
+                </tr>
+                <tr>
+                    <td>Vera</td>
+                    <td>Heins</td>
+                    <td>viool 1</td>
+                </tr>
+                <tr>
+                    <td><i>vacature</i></td>
+                    <td></td>
+                    <td>viool 2 (aanvoerder)</td>
+                </tr>
+                <tr>
+                    <td>Adriaan</td>
+                    <td>Mol</td>
+                    <td>viool 2</td>
+                </tr>
+                <tr>
+                    <td>Bea</td>
+                    <td>Selman</td>
+                    <td>viool 2</td>
+                </tr>
+                <tr>
+                    <td>Daniela</td>
+                    <td>Roeland</td>
+                    <td>viool 2</td>
+                </tr>
+                <tr>
+                    <td>Jan</td>
+                    <td>Prins</td>
+                    <td>viool 2</td>
+                </tr>
+                <tr>
+                    <td>Eline</td>
+                    <td>Snoek</td>
+                    <td>altviool (aanvoerder)</td>
+                </tr>
+                <tr>
+                    <td>Marijke</td>
+                    <td>Huijgen</td>
+                    <td>altviool</td>
+                </tr>
+                <tr>
+                    <td>Saskia</td>
+                    <td>Wieberdink</td>
+                    <td>altviool</td>
+                </tr>
+                <tr>
+                    <td>Peter</td>
+                    <td>Klusener</td>
+                    <td>altviool</td>
+                </tr>
+                <tr>
+                    <td>Gill</td>
+                    <td>Griffiths</td>
+                    <td>cello</td>
+                </tr>
+                <tr>
+                    <td>Jan</td>
+                    <td>Strumphler</td>
+                    <td>cello</td>
+                </tr>
+                <tr>
+                    <td>Robin</td>
+                    <td>Gerretsen</td>
+                    <td>cello</td>
+                </tr>
+                <tr>
+                    <td>Anke</td>
+                    <td>Meesters</td>
+                    <td>cello (aanvoerder)</td>
+                </tr>
+                <tr>
+                    <td>Ernő</td>
+                    <td>Groeneweg</td>
+                    <td>contrabas</td>
+                </tr>
+            </tbody>
+        </table>
+        <p>&nbsp;</p>
     </div>
 </body>
 
