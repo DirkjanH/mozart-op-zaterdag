7d47990e8bb2f1f88471b196352508134fd7bdad
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-05-26 12:55:19 +0200

    Update concert details for KV 491 and add new KV 417 concert page

---
+++ 2026-09-26/kv417.php
@@ -0,0 +1,261 @@
+<?php require_once '../includes/inloggen.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Concert voor fluit en harp in C KV 417</title>
+    <link href="/css/moz.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div class="w3-content w3-white w3-panel">
+        <?php require_once '../navigatie.htm'; ?>
+        <h3>Mozart op Zaterdag op 26 september 2026:</h3>
+        <h1>Concert voor fluit en harp in C KV 417</h1>
+        <p>
+            Op zaterdagochtend 26 september speelt <i>Mozart op Zaterdag</i> zijn
+            <strong>Concert voor fluit en harp in C KV 417</strong>. Dit werk behoort tot
+            Mozarts meest indrukwekkende en dramatische concerten, en geldt als een van
+            zijn twee pianoconcerten in een mineurtoonsoort. Mozart voltooide het in 1786,
+            in een periode waarin hij op het toppunt van zijn creativiteit stond.
+        </p>
+        <p>Het concert duurt ca. 30 minuten. De bezetting is 2 hobo's, 1 fagot, 2 hoorns en strijkers. 
+            De partijen vind je hieronder. We doen alle herhalingen. De tempi worden:</p>
+            <ol>
+                <li>Allegro: kwart = 124</li>
+                <li>Andantino: achtste = ca. 96</li>
+                <li>Rondo Allegro: halve noot = 72; in de gedeelte in 6/8 wordt het 100 voor de gepuncteerde kwart</li>
+            </ol>
+
+        <h3>Partijen</h3>
+        <ul style="column-count: 3;">
+            <li>
+                <a href="\2026-05-23\K491.Flute.pdf" target="_blank">Dwarsfluit</a>
+            </li>
+            <li>
+                <a href="\2026-05-23\K491.Oboe.pdf" target="_blank">Hobo 1/2</a>
+            </li>
+            <li>
+                <a href="\2026-05-23\K491.Clarinet.pdf" target="_blank">Klarinet 1/2</a>
+            </li>
+            <li>
+                <a href="\2026-05-23\K491.Bassoon.pdf" target="_blank">Fagot 1/2</a>
+            </li>
+            <li>
+                <a href="\2026-05-23\K491.Horn.pdf" target="_blank">Hoorn 1/2</a>
+            </li>
+            <li>
+                <a href="\2026-05-23\K491.Trumpet.pdf" target="_blank">Trompet 1/2</a>
+            </li>
+            <li>
+                <a href="\2026-05-23\K491.Timpani.pdf" target="_blank">Pauken</a>
+            </li>
+            <li>
+                <a href="\2026-05-23\K491.Violin1_betekend.pdf" target="_blank">Viool 1 (betekend)</a>
+            </li>
+            <li>
+                <a href="\2026-05-23\K491.Violin2_betekend.pdf" target="_blank">Viool 2 (betekend)</a>
+            </li>
+            <li>
+                <a href="\2026-05-23\K491.Viola_betekend.PDF" target="_blank">Altviool (betekend)</a>
+            </li>
+            <li>
+                <a href="\2026-05-23\K491.Cello_betekend.pdf" target="_blank">Cello/contrabas (betekend)</a>
+            </li>
+            <li>
+                <a href="\2026-05-23\K491_score.pdf" target="_blank">Partituur</a>
+            </li>
+        </ul>
+        <div class="">
+            <h2>Bezetting</h2>
+            <p>Er zijn nu 32 deelnemers. Er is nog plaats voor een tweede viool.</p>
+            <p class="onzichtbaar">Binnenkort plaatsen we hier de lijst van alle deelnemers.</p>
+            <table class="w3-table w3-striped w3-bordered" id="deelnemers">
+                <thead>
+                    <tr>
+                        <th>voornaam</th>
+                        <th>achternaam</th>
+                        <th>instrument</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Laurie</td>
+                        <td>Boltjes</td>
+                        <td>fluit</td>
+                    </tr>
+                    <tr>
+                        <td>Erik</td>
+                        <td>van Kassel</td>
+                        <td>hobo 1</td>
+                    </tr>
+                    <tr>
+                        <td>Eva</td>
+                        <td>Elgeti</td>
+                        <td>hobo 2</td>
+                    </tr>
+                    <tr>
+                        <td>Marcus </td>
+                        <td>Prins</td>
+                        <td>klarinet 1</td>
+                    </tr>
+                    <tr>
+                        <td>Inge</td>
+                        <td>Out </td>
+                        <td>klarinet 2</td>
+                    </tr>
+                    <tr>
+                        <td>Yolande </td>
+                        <td>Krooshof</td>
+                        <td>fagot 1</td>
+                    </tr>
+                    <tr>
+                        <td>Pieter</td>
+                        <td>de Oude</td>
+                        <td>fagot 2</td>
+                    </tr>
+                    <tr>
+                        <td>Richard</td>
+                        <td>van Hoorn</td>
+                        <td>hoorn 1</td>
+                    </tr>
+                    <tr>
+                        <td>Wim</td>
+                        <td>Kuppen</td>
+                        <td>hoorn 2</td>
+                    </tr>
+                    <tr>
+                        <td>Willem</td>
+                        <td>Naert</td>
+                        <td>trompet 1</td>
+                    </tr>
+                    <tr>
+                        <td>Judit</td>
+                        <td></td>
+                        <td>trompet 2</td>
+                    </tr>
+                    <tr>
+                        <td>Agnes</td>
+                        <td>Hoeksma</td>
+                        <td>pauken</td>
+                    </tr>
+                    <tr>
+                        <td>Carla</td>
+                        <td>Van Kassel-Westhof</td>
+                        <td>viool 1 (concertmeester)</td>
+                    </tr>
+                    <tr>
+                        <td>Janne</td>
+                        <td>den Blaauwen </td>
+                        <td>viool 1</td>
+                    </tr>
+                    <tr>
+                        <td>Rudolf</td>
+                        <td>Rasch</td>
+                        <td>viool 1</td>
+                    </tr>
+                    <tr>
+                        <td>Jan</td>
+                        <td>Prins</td>
+                        <td>viool 1</td>
+                    </tr>
+                    <tr>
+                        <td>Petra</td>
+                        <td>van Besouw</td>
+                        <td>viool 1</td>
+                    </tr>
+                    <tr>
+                        <td>Paul</td>
+                        <td>van Coeverden</td>
+                        <td>viool 1</td>
+                    </tr>
+                    <tr>
+                        <td>Frits </td>
+                        <td>Muusse </td>
+                        <td>viool 2</td>
+                    </tr>
+                    <tr>
+                        <td>Ruben</td>
+                        <td>Faas</td>
+                        <td>viool 2</td>
+                    </tr>
+                    <tr>
+                        <td><i>vacature</i></td>
+                        <td></td>
+                        <td>viool 2</td>
+                    </tr>
+                    <tr>
+                        <td>Bea</td>
+                        <td>Selman</td>
+                        <td>viool 2</td>
+                    </tr>
+                    <tr>
+                        <td>Saskia</td>
+                        <td>Wieberdink</td>
+                        <td>viool 2 (aanvoerder)</td>
+                    </tr>
+                    <tr>
+                        <td>Adriaan</td>
+                        <td>Mol</td>
+                        <td>viool 2</td>
+                    </tr>
+                    <tr>
+                        <td>Marijke</td>
+                        <td>Huijgen</td>
+                        <td>altviool (aanvoerder)</td>
+                    </tr>
+                    <tr>
+                        <td>Peter</td>
+                        <td>Klusener</td>
+                        <td>altviool</td>
+                    </tr>
+                    <tr>
+                        <td>Lia</td>
+                        <td>van Schaijk</td>
+                        <td>altviool</td>
+                    </tr>
+                    <tr>
+                        <td>Maria</td>
+                        <td>Voorhout</td>
+                        <td>altviool</td>
+                    </tr>
+                    <tr>
+                        <td>Meike</td>
+                        <td>Jongejan </td>
+                        <td>cello (aanvoerder)</td>
+                    </tr>
+                    <tr>
+                        <td>Nicoline</td>
+                        <td>Simonis</td>
+                        <td>cello</td>
+                    </tr>
+                    <tr>
+                        <td>Franca</td>
+                        <td>Post</td>
+                        <td>cello</td>
+                    </tr>
+                    <tr>
+                        <td>Jan</td>
+                        <td>Strumphler</td>
+                        <td>cello</td>
+                    </tr>
+                    <tr>
+                        <td>Jurgen</td>
+                        <td>van der Ent</td>
+                        <td>contrabas</td>
+                    </tr>
+                    <tr>
+                        <td>Anke</td>
+                        <td>Muusse</td>
+                        <td>contrabas</td>
+                    </tr>
+            </table>
+        </div>
+    </div>
+    <p>&nbsp;</p>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -48,12 +48,12 @@
             <li class="onzichtbaar">
                 <b>25 april:</b> Symfonie nr. 29 in A KV 201 voor 2 hobo's, 1 fagot, 2 hoorns en strijkers. <a href="/2026-04-25/kv201.php" target="_blank">Meer info & partijen vind je hier</a>.
             </li>
-            <li>
+            <li class="onzichtbaar">
                 <b>23 mei:</b> Pianoconcert nr. 24 in c klein KV 491 voor 1 fluit,
                 2 hobo's, 2 klarinetten, 2 fagotten, 2 hoorns, 2 trompetten, pauken en strijkers. Solist is de pianist Hans-Erik Dijkstra. Er is nog plaats voor een of twee 1e violen en <b>twee trompetten</b>. <a href="/2026-05-23/kv491.php" target="_blank">Meer info & partijen vind je hier</a>.
             </li>
             <li>
-                <b>26 september:</b> Concert voor fluit en harp in C KV 417 voor 2 hobo's, 1 fagot, 2 hoorns en strijkers. Solisten zijn: Elisa Bartolomé Gómez, dwarsfluit, en Maria Palma, harp. <a xxxx="/2026-09-26/Moz_2026-09-26.php" target="_blank">Meer info & partijen vind je binnenkort hier</a>.
+                <b>26 september:</b> Concert voor fluit en harp in C KV 417 voor 2 hobo's, 1 fagot, 2 hoorns en strijkers. Solisten zijn: Elisa Bartolomé Gómez, dwarsfluit, en Maria Palma, harp. <a href="/2026-09-26/kv417.php" target="_blank">Meer info & partijen vind je hier</a>.
             </li>
             <li>
                 <b>24 oktober:</b> “Parijse” Ouverture in D KV 311a & “Parijse” Symfonie nr. 31 in D KV 297 voor 2 fluiten,
