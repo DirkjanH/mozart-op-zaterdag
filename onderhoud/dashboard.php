5b570f151829d842b25d4fd5080b9af8e30159ee
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-08-25 16:03:43 +0200

    Update dashboard title and button styling for consistency

--- onderhoud/dashboard.php
+++ onderhoud/dashboard.php
@@ -6,18 +6,19 @@ require_once __DIR__ . '/../includes/inloggen.php';
 
 <head>
     <meta charset="UTF-8">
-    <title>Mozart op Zaterdag - Beheer</title>
+    <title>MoZ dashboard</title>
     <link href="/css/moz.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
     <div class="w3-content w3-mobile w3-white w3-panel" style="max-width:700px;">
-        <h3>Mozart op Zaterdag - Beheer</h3>
+        <h3>Mozart op Zaterdag - Dasboard</h3>
         <div class="w3-bar-block">
             <a href="deelnemers_bewerken.php" target="_blank" class="w3-bar-item w3-button w3-blue w3-margin-bottom">Deelnemers bewerken</a>
             <a href="werken_bewerken.php" target="_blank" class="w3-bar-item w3-button w3-blue w3-margin-bottom">Werken bewerken</a>
             <a href="activiteiten_bewerken.php" target="_blank" class="w3-bar-item w3-button w3-blue w3-margin-bottom">Activiteiten bewerken</a>
-            <a href="beschikbaarheid.php" target="_blank" class="w3-bar-item w3-button w3-light-grey" title="Nog te maken">Beschikbaarheid overzicht</a>
+            <a href="beschikbaarheid.php" target="_blank" class="w3-bar-item w3-button w3-blue w3-margin-bottom">Beschikbaarheid overzicht</a>
+            <a href="genereer_webpaginas.php" target="_blank" class="w3-bar-item w3-button w3-light-grey" title="Nog te maken">Genereer webpagina's</a>
         </div>
     </div>
 </body>
