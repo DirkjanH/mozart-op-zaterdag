7d9d7f1d10e23d19089ab29c34e7b0289b10afb1
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-08-25 14:55:19 +0200

    Update dashboard.php to open management links in a new tab

--- onderhoud/dashboard.php
+++ onderhoud/dashboard.php
@@ -14,10 +14,10 @@ require_once __DIR__ . '/../includes/inloggen.php';
     <div class="w3-content w3-mobile w3-white w3-panel" style="max-width:700px;">
         <h3>Mozart op Zaterdag - Beheer</h3>
         <div class="w3-bar-block">
-            <a href="deelnemers_bewerken.php" class="w3-bar-item w3-button w3-blue w3-margin-bottom">Deelnemers bewerken</a>
-            <a href="werken_bewerken.php" class="w3-bar-item w3-button w3-blue w3-margin-bottom">Werken bewerken</a>
-            <a href="activiteiten_bewerken.php" class="w3-bar-item w3-button w3-blue w3-margin-bottom">Activiteiten bewerken</a>
-            <a href="beschikbaarheid.php" class="w3-bar-item w3-button w3-light-grey" title="Nog te maken">Beschikbaarheid overzicht</a>
+            <a href="deelnemers_bewerken.php" target="_blank" class="w3-bar-item w3-button w3-blue w3-margin-bottom">Deelnemers bewerken</a>
+            <a href="werken_bewerken.php" target="_blank" class="w3-bar-item w3-button w3-blue w3-margin-bottom">Werken bewerken</a>
+            <a href="activiteiten_bewerken.php" target="_blank" class="w3-bar-item w3-button w3-blue w3-margin-bottom">Activiteiten bewerken</a>
+            <a href="beschikbaarheid.php" target="_blank" class="w3-bar-item w3-button w3-light-grey" title="Nog te maken">Beschikbaarheid overzicht</a>
         </div>
     </div>
 </body>
