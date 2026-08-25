86235176b5e414a826cf9ddda2af05bfe1f75be4
Dirkjan Horringa <dirkjan@pellegrina.net>
2026-08-25 14:09:59 +0200

    Add dashboard.php for management interface with navigation links

---
+++ onderhoud/dashboard.php
@@ -0,0 +1,25 @@
+<?php
+require_once __DIR__ . '/../includes/inloggen.php';
+?>
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Mozart op Zaterdag - Beheer</title>
+    <link href="/css/moz.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div class="w3-content w3-mobile w3-white w3-panel" style="max-width:700px;">
+        <h3>Mozart op Zaterdag - Beheer</h3>
+        <div class="w3-bar-block">
+            <a href="deelnemers_bewerken.php" class="w3-bar-item w3-button w3-blue w3-margin-bottom">Deelnemers bewerken</a>
+            <a href="werken_bewerken.php" class="w3-bar-item w3-button w3-blue w3-margin-bottom">Werken bewerken</a>
+            <a href="activiteiten_bewerken.php" class="w3-bar-item w3-button w3-light-grey w3-margin-bottom" title="Nog te maken">Activiteiten bewerken</a>
+            <a href="beschikbaarheid.php" class="w3-bar-item w3-button w3-light-grey" title="Nog te maken">Beschikbaarheid overzicht</a>
+        </div>
+    </div>
+</body>
+
+</html>
